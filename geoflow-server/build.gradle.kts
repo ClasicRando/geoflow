9be434b58ea5057f052d351eb9beefb07eed0ff8
Steven <steventhomson72@yahoo.ca>
2021-09-05 00:25:30 -0400

    added static logger dependency

--- geoflow-server/build.gradle.kts
+++ geoflow-server/build.gradle.kts
@@ -4,12 +4,14 @@ plugins {
     kotlin("jvm") version "1.5.21"
     application
     kotlin("plugin.serialization") version "1.5.21"
+    id("com.github.johnrengelman.shadow") version "7.0.0"
 }
 
 group = "me.steven"
 version = "1.0-SNAPSHOT"
 val ktorVersion = "1.6.3"
 val htmlJvmVersion = "0.7.3"
+val slfVersion = "1.7.32"
 
 dependencies {
     implementation(project(":geoflow-core"))
@@ -20,6 +22,10 @@ dependencies {
     implementation("io.ktor:ktor-auth:$ktorVersion")
     implementation("io.ktor:ktor-serialization:$ktorVersion")
     implementation("org.jetbrains.kotlinx:kotlinx-html-jvm:$htmlJvmVersion")
+    // https://mvnrepository.com/artifact/org.slf4j/slf4j-api
+    implementation("org.slf4j:slf4j-api:$slfVersion")
+    // https://mvnrepository.com/artifact/org.slf4j/slf4j-api
+    implementation("org.slf4j:slf4j-simple:$slfVersion")
 }
 
 tasks.test {
@@ -32,4 +38,12 @@ tasks.withType<KotlinCompile>() {
 
 application {
     mainClass.set("ServerKt")
+}
+
+tasks{
+    shadowJar {
+        manifest {
+            attributes(Pair("Main-Class", application.mainClass))
+        }
+    }
 }
