249f2cdb774d085d81009861fc5b3ebe5e6868dc
Steven <steventhomson72@yahoo.ca>
2021-09-11 00:33:20 -0400

    added javascript functions

---
+++ geoflow-server/src/main/kotlin/html/javascript.kt
@@ -0,0 +1,29 @@
+package html
+
+import kotlinx.html.FlowContent
+import kotlinx.html.script
+import kotlinx.html.unsafe
+
+val FlowContent.postObject get() = this.script {
+    unsafe {
+        raw("""
+            function post(params) {
+                    const form = document.createElement('form');
+                    form.method = 'post';
+                    form.action = '';
+                    
+                    for (const [key, value] of Object.entries(params)) {
+                        const hiddenField = document.createElement('input');
+                        hiddenField.type = 'hidden';
+                        hiddenField.name = key;
+                        hiddenField.value = value;
+                        
+                        form.appendChild(hiddenField);
+                    }
+                    
+                    document.body.appendChild(form);
+                    form.submit();
+                }
+        """.trimIndent())
+    }
+}
