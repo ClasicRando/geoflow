4e4b1376ba06364feac0d6bb25e55ed0594e013d
Steven <steventhomson72@yahoo.ca>
2021-09-11 00:36:28 -0400

    added api route

--- geoflow-server/src/main/kotlin/routes.kt
+++ geoflow-server/src/main/kotlin/routes.kt
@@ -1,8 +1,13 @@
+import auth.UserSession
 import html.index
 import io.ktor.application.*
 import io.ktor.html.*
+import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
+import io.ktor.sessions.*
+import orm.tables.Actions
+import orm.tables.WorkflowOperations
 
 fun Route.index() {
     get("/") {
@@ -14,6 +19,29 @@ fun Route.index() {
         }
     }
     post("/index") {
+        val params = call.receiveParameters()
+        val href = params["href"] ?: "/index"
+        call.respondRedirect(href)
+    }
+}
 
+fun Route.api() {
+    get("/api/operations") {
+        val operations = runCatching {
+            WorkflowOperations.userOperations(call.sessions.get<UserSession>()?.roles!!)
+        }.getOrElse { t ->
+            call.application.environment.log.error("/api/operations", t)
+            listOf()
+        }
+        call.respond(operations)
+    }
+    get("/api/actions") {
+        val actions = runCatching {
+            Actions.userActions(call.sessions.get<UserSession>()?.roles!!)
+        }.getOrElse { t ->
+            call.application.environment.log.error("/api/actions", t)
+            listOf()
+        }
+        call.respond(actions)
     }
 }
