131680ad65940faa2cb3779055f71925f8723064
Steven <steventhomson72@yahoo.ca>
2021-09-11 00:35:34 -0400

    updated index page with tables

--- geoflow-server/src/main/kotlin/html/Index.kt
+++ geoflow-server/src/main/kotlin/html/Index.kt
@@ -1,15 +1,45 @@
 package html
 
-import io.ktor.html.*
-import kotlinx.html.FlowContent
+import kotlinx.html.div
+import kotlinx.html.script
+import kotlinx.html.unsafe
+import orm.tables.Actions
+import orm.tables.WorkflowOperations
 
 class Index: BasePage() {
+    private val operationsTableId = "operations"
+    private val actionsTableId = "actions"
     init {
         setContent {
-            +"This is the index"
+            div(classes = "row") {
+                div(classes = "col") {
+                    basicTable(
+                        "Operations",
+                        operationsTableId,
+                        "/api/operations",
+                        WorkflowOperations.tableDisplayFields
+                    )
+                }
+                div(classes = "col") {
+                    basicTable(
+                        "Actions",
+                        actionsTableId,
+                        "/api/actions",
+                        Actions.tableDisplayFields
+                    )
+                }
+            }
         }
         setScript {
-
+            postObject
+            script {
+                unsafe {
+                    raw("""
+                        $('#$operationsTableId').on('click-row.bs.table', (e, row, element, field) => { post(row) });
+                        $('#$actionsTableId').on('click-row.bs.table', (e, row, element, field) => { post(row) });
+                    """.trimIndent())
+                }
+            }
         }
     }
 }
