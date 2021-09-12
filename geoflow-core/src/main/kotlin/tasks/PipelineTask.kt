0c79bbff16c68b4255e57fe2e851127ad1238942
Steven <steventhomson72@yahoo.ca>
2021-09-12 01:06:37 -0400

    changed to more of a base task implementing and requiring less

--- geoflow-core/src/main/kotlin/tasks/PipelineTask.kt
+++ geoflow-core/src/main/kotlin/tasks/PipelineTask.kt
@@ -1,28 +1,11 @@
 package tasks
 
-import database.DatabaseConnection
+import orm.entities.PipelineRunTask
 import orm.tables.PipelineRunTasks
-import java.time.Instant
 
-abstract class PipelineTask {
+abstract class PipelineTask(val pipelineRunTaskId: Long) {
 
+    protected val task: PipelineRunTask = PipelineRunTasks.getRecord(pipelineRunTaskId)
     abstract val taskId: Long
-    abstract val pipelineRunTaskId: Long
-    abstract suspend fun task()
-
-    suspend fun run() {
-        DatabaseConnection.database.useTransaction {
-            val taskRecord = PipelineRunTasks.reserveRecord(pipelineRunTaskId)
-            taskRecord.taskStart = Instant.now()
-            taskRecord.taskRunning = true
-            taskRecord.taskComplete = false
-            taskRecord.taskCompleted = null
-            taskRecord.flushChanges()
-            task()
-            taskRecord.taskRunning = false
-            taskRecord.taskComplete = true
-            taskRecord.taskCompleted = Instant.now()
-            taskRecord.flushChanges()
-        }
-    }
+    abstract suspend fun runTask()
 }
