39854679ae381c24ed6b4c7b0c8f359ad7d57e5c
Steven <steventhomson72@yahoo.ca>
2021-09-11 23:57:56 -0400

    made some fields updatable

--- geoflow-core/src/main/kotlin/orm/entities/PipelineRunTask.kt
+++ geoflow-core/src/main/kotlin/orm/entities/PipelineRunTask.kt
@@ -6,10 +6,10 @@ import java.time.Instant
 interface PipelineRunTask: Entity<PipelineRunTask> {
     val pipelineRunTaskId: Long
     val runId: Long
-    val taskRunning: Boolean
-    val taskComplete: Boolean
-    val taskStart: Instant
-    val taskCompleted: Instant
+    var taskRunning: Boolean
+    var taskComplete: Boolean
+    var taskStart: Instant?
+    var taskCompleted: Instant?
     val taskId: Long?
     val taskMessage: String?
 }
