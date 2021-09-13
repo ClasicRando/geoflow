699c9c0a5615c06adb44ccb2331bf852ee76b4a3
Steven <steventhomson72@yahoo.ca>
2021-09-13 02:46:10 -0400

    updated and added new routes

--- geoflow-server/src/main/kotlin/routes.kt
+++ geoflow-server/src/main/kotlin/routes.kt
@@ -1,7 +1,5 @@
 import auth.UserSession
-import html.accessRestricted
-import html.index
-import html.pipelineStatus
+import html.*
 import io.ktor.application.*
 import io.ktor.html.*
 import io.ktor.request.*
@@ -9,6 +7,7 @@ import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.sessions.*
 import orm.tables.Actions
+import orm.tables.PipelineRunTasks
 import orm.tables.PipelineRuns
 import orm.tables.WorkflowOperations
 
@@ -65,6 +64,42 @@ fun Route.pipelineStatus() {
             }
         }
     }
+    get("/pipeline-status") {
+        val code = call.request.queryParameters["code"] ?: ""
+        call.respondHtml {
+            if (call.sessions.get<UserSession>()!!.hasRole(code)) {
+                pipelineStatus(code)
+            } else {
+                accessRestricted(code)
+            }
+        }
+    }
+    post("/pipeline-status") {
+        val runId = call.receiveParameters()["run_id"] ?: ""
+        call.respondRedirect("/tasks?runId=$runId")
+    }
+}
+
+fun Route.pipelineTasks() {
+    get("/tasks") {
+        val runIdStr = call.request.queryParameters["runId"] ?: "0"
+        val runId = runIdStr.toLong()
+        val run = PipelineRuns.getRun(runId)
+        val runUsernames = listOf(
+            run?.collectionUser?.username,
+            run?.loadUser?.username,
+            run?.checkUser?.username,
+            run?.qaUser?.username,
+        ).mapNotNull { it }
+        val user = call.sessions.get<UserSession>()!!
+        when {
+            run == null -> call.respondHtml { invalidParameter("Run ID provided cannot be found") }
+            user.hasRole("admin") || user.username in runUsernames -> {
+                call.respondHtml { pipelineTasks(runId) }
+            }
+            else -> call.respondHtml { invalidParameter("You must be a part of this run to view it") }
+        }
+    }
 }
 
 fun Route.api() {
@@ -86,16 +121,25 @@ fun Route.api() {
         }
         call.respond(actions)
     }
-    get("api/pipeline-runs") {
+    get("/api/pipeline-runs") {
         val runs = runCatching {
             PipelineRuns.userRuns(
                 call.sessions.get<UserSession>()?.userId!!,
                 call.request.queryParameters["code"] ?: ""
             )
         }.getOrElse { t ->
-            call.application.environment.log.error("/api/actions", t)
+            call.application.environment.log.error("/api/pipeline-runs", t)
             listOf()
         }
         call.respond(runs)
+    }
+    get("/api/pipeline-run-tasks") {
+        val tasks = runCatching {
+            PipelineRunTasks.getTasks(call.request.queryParameters["taskId"]?.toLong() ?: 0)
+        }.getOrElse { t ->
+            call.application.environment.log.error("/api/pipeline-run-tasks", t)
+            listOf()
+        }
+        call.respond(tasks)
     }
 }
