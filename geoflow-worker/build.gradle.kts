cc3ed376fcdcee9992aeea3f7eb5ead0801d8d01
Steven <steventhomson72@yahoo.ca>
2021-09-12 23:06:44 -0400

    added coroutine and logger dependencies

--- geoflow-worker/build.gradle.kts
+++ geoflow-worker/build.gradle.kts
@@ -1,11 +1,12 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
     application
 }
 
 version = "0.1"
-val kjobVersion = "0.2.0"
+val kjobVersion: String by project
+val coroutinesVersion: String by project
+val slf4Version: String by project
+val kotlinLoggingVersion: String by project
 
 dependencies {
     implementation(project(":geoflow-core"))
@@ -14,6 +15,13 @@ dependencies {
     implementation("it.justwrote:kjob-core:$kjobVersion")
     // https://mvnrepository.com/artifact/it.justwrote/kjob-mongo
     implementation("it.justwrote:kjob-mongo:$kjobVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:$coroutinesVersion")
+    // https://mvnrepository.com/artifact/org.slf4j/slf4j-api
+    implementation("org.slf4j:slf4j-api:$slf4Version")
+    // https://mvnrepository.com/artifact/org.slf4j/slf4j-simple
+    implementation("org.slf4j:slf4j-simple:$slf4Version")
+    implementation("io.github.microutils:kotlin-logging-jvm:$kotlinLoggingVersion")
 }
 
 tasks.test {
