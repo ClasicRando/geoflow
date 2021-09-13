3a849ab92eae9b858792ee5e2a52cab49e8491e2
Steven <steventhomson72@yahoo.ca>
2021-09-13 02:41:48 -0400

    added page template for viewing pipeline run tasks

---
+++ geoflow-server/src/main/kotlin/html/PipelineTasks.kt
@@ -0,0 +1,20 @@
+package html
+
+import orm.tables.PipelineRunTasks
+
+class PipelineTasks(runId: Long): BasePage() {
+    init {
+        setContent {
+            basicTable(
+                "Tasks",
+                "tasks",
+                "/api/pipeline-run-tasks?taskId=$runId",
+                PipelineRunTasks.tableDisplayFields,
+                buttons = listOf("btnRun")
+            )
+        }
+        setScript {
+
+        }
+    }
+}
