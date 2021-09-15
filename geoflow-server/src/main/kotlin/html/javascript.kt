107c65ed4d989a43c0e87100c58edfa9beeac31f
Steven <steventhomson72@yahoo.ca>
2021-09-14 21:19:13 -0400

    added function to post to url

--- geoflow-server/src/main/kotlin/html/javascript.kt
+++ geoflow-server/src/main/kotlin/html/javascript.kt
@@ -8,22 +8,41 @@ val FlowContent.postObject get() = this.script {
     unsafe {
         raw("""
             function post(params) {
-                    const form = document.createElement('form');
-                    form.method = 'post';
-                    form.action = '';
+                const form = document.createElement('form');
+                form.method = 'post';
+                form.action = '';
+                
+                for (const [key, value] of Object.entries(params)) {
+                    const hiddenField = document.createElement('input');
+                    hiddenField.type = 'hidden';
+                    hiddenField.name = key;
+                    hiddenField.value = value;
                     
-                    for (const [key, value] of Object.entries(params)) {
-                        const hiddenField = document.createElement('input');
-                        hiddenField.type = 'hidden';
-                        hiddenField.name = key;
-                        hiddenField.value = value;
-                        
-                        form.appendChild(hiddenField);
-                    }
-                    
-                    document.body.appendChild(form);
-                    form.submit();
+                    form.appendChild(hiddenField);
                 }
+                
+                document.body.appendChild(form);
+                form.submit();
+            }
         """.trimIndent())
+    }
+}
+
+fun FlowContent.postValue() {
+    script {
+        unsafe {
+            raw("""
+                function postValue(url) {
+                    const options = {
+                        method: 'POST',
+                    };
+                    fetch(url, options).then(result => {
+                        result.json()
+                    }).then(json => {
+                        console.log(json);
+                    });
+                }
+            """.trimIndent())
+        }
     }
 }
