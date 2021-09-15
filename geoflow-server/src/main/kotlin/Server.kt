42d22a44971f55dc7d0ff8b5ad355ad4b59a70e9
Steven <steventhomson72@yahoo.ca>
2021-09-14 21:15:24 -0400

    added kjob scheduler

--- geoflow-server/src/main/kotlin/Server.kt
+++ geoflow-server/src/main/kotlin/Server.kt
@@ -10,8 +10,32 @@ import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.serialization.*
 import io.ktor.sessions.*
+import it.justwrote.kjob.Mongo
+import it.justwrote.kjob.job.JobExecutionType
+import it.justwrote.kjob.kjob
+import mu.KotlinLogging
 import orm.tables.InternalUsers
 import java.time.Instant
+
+val logger = KotlinLogging.logger {}
+val kjob = kjob(Mongo) {
+    nonBlockingMaxJobs = 1
+    blockingMaxJobs = 1
+    maxRetries = 3
+    defaultJobExecutor = JobExecutionType.NON_BLOCKING
+
+    exceptionHandler = { t -> logger.error("Unhandled exception", t)}
+    keepAliveExecutionPeriodInSeconds = 60
+    jobExecutionPeriodInSeconds = 1
+    cleanupPeriodInSeconds = 300
+    cleanupSize = 50
+
+    connectionString = "mongodb://127.0.0.1:27017"
+    databaseName = "kjob"
+    jobCollection = "kjob-jobs"
+    lockCollection = "kjob-locks"
+    expireLockInMinutes = 5L
+}.start()
 
 fun main(args: Array<String>): Unit = io.ktor.server.cio.EngineMain.main(args)
 
