42990ab31778a05f7d1587c26d3b3ff35f0a8b83
Steven <steventhomson72@yahoo.ca>
2021-09-14 21:16:13 -0400

    added function to get a user task

--- geoflow-core/src/main/kotlin/utils.kt
+++ geoflow-core/src/main/kotlin/utils.kt
@@ -1,3 +1,6 @@
+import orm.entities.Task
+import tasks.SystemTask
+import tasks.UserTask
 import java.time.Instant
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
@@ -5,3 +8,11 @@ import java.time.format.DateTimeFormatter
 fun formatInstantDefault(timestamp: Instant?) = timestamp
     ?.atZone(ZoneId.systemDefault())
     ?.format(DateTimeFormatter.ISO_LOCAL_DATE) ?: ""
+
+fun getUserPipelineTask(pipelineRunTaskId: Long, task: Task): UserTask {
+    return ClassLoader
+        .getSystemClassLoader()
+        .loadClass(task.taskClassName)
+        .getConstructor(Long::class.java)
+        .newInstance(pipelineRunTaskId) as UserTask
+}
