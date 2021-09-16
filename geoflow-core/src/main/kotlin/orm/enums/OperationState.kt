f02f38a8c5ae7a54c7e2e2f433501b5f6832c080
Steven <steventhomson72@yahoo.ca>
2021-09-15 22:31:39 -0400

    added create statement to enums

--- geoflow-core/src/main/kotlin/orm/enums/OperationState.kt
+++ geoflow-core/src/main/kotlin/orm/enums/OperationState.kt
@@ -1,5 +1,10 @@
 package orm.enums
 
+/**
+ * Enum type found in DB denoting the current state of a run
+ * CREATE TYPE public.operation_state AS ENUM
+ * ('Ready', 'Active');
+ */
 enum class OperationState {
     Ready,
     Active,
--- geoflow-core/src/main/kotlin/orm/enums/TaskStatus.kt
+++ geoflow-core/src/main/kotlin/orm/enums/TaskStatus.kt
@@ -1,5 +1,10 @@
 package orm.enums
 
+/**
+ * Enum type found in DB denoting the task status
+ * CREATE TYPE public.task_status AS ENUM
+ * ('Waiting', 'Scheduled', 'Running', 'Complete');
+ */
 enum class TaskStatus {
     Waiting,
     Scheduled,
