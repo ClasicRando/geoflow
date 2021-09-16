c9dab726c3fdc57cb309678b25a258f8057d2128
Steven <steventhomson72@yahoo.ca>
2021-09-15 22:55:39 -0400

    added new columns to table

--- geoflow-core/src/main/kotlin/orm/entities/PipelineTask.kt
+++ geoflow-core/src/main/kotlin/orm/entities/PipelineTask.kt
@@ -5,4 +5,6 @@ import org.ktorm.entity.Entity
 interface PipelineTask: Entity<PipelineTask> {
     val pipelineId: Long
     val taskId: Long
+    val parentTask: Long
+    val parentTaskOrder: Int
 }
--- geoflow-core/src/main/kotlin/orm/tables/PipelineTasks.kt
+++ geoflow-core/src/main/kotlin/orm/tables/PipelineTasks.kt
@@ -1,12 +1,15 @@
 package orm.tables
 
 import org.ktorm.schema.Table
+import org.ktorm.schema.int
 import org.ktorm.schema.long
 import orm.entities.PipelineTask
 
 object PipelineTasks: Table<PipelineTask>("pipeline_tasks") {
     val pipelineId = long("pipeline_id").primaryKey().bindTo { it.pipelineId }
     val taskId = long("task_id").primaryKey().bindTo { it.taskId }
+    val parentTask = long("parent_task").bindTo { it.parentTask }
+    val parentTaskOrder = int("parent_task_order").bindTo { it.parentTaskOrder }
 
     val createStatement = """
         CREATE TABLE IF NOT EXISTS public.pipeline_tasks
