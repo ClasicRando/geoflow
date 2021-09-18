31ce8a42b4845a76bc08ad80c1536e2cb7e1bba0
Steven <steventhomson72@yahoo.ca>
2021-09-18 15:09:25 -0400

    added sequence for source tables

--- geoflow-core/src/main/kotlin/database/sequences.kt
+++ geoflow-core/src/main/kotlin/database/sequences.kt
@@ -3,5 +3,7 @@ package database
 import org.ktorm.database.Database
 import org.ktorm.entity.sequenceOf
 import orm.tables.Roles
+import orm.tables.SourceTables
 
 val Database.roles get() = this.sequenceOf(Roles)
+val Database.sourceTables get() = this.sequenceOf(SourceTables)
