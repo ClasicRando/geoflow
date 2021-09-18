5e4763ec502b361c4165ec728237f990f6d9b41b
Steven <steventhomson72@yahoo.ca>
2021-09-18 13:33:27 -0400

    added action for task

--- geoflow-core/src/main/kotlin/tasks/ValidateFirstPipeline.kt
+++ geoflow-core/src/main/kotlin/tasks/ValidateFirstPipeline.kt
@@ -1,11 +1,23 @@
 package tasks
 
+import database.DatabaseConnection
+import org.ktorm.dsl.*
+import orm.tables.SourceTables
+
 class ValidateFirstPipeline(pipelineRunTaskId: Long): SystemTask(pipelineRunTaskId) {
 
     override val taskId: Long = 11
 
     override suspend fun run() {
-
+        DatabaseConnection.database.run {
+            val sourceTableCount = from(SourceTables)
+                .select(count())
+                .where { SourceTables.runId eq task.runId }
+                .map { row -> row.getInt(1) }
+                .first()
+            if (sourceTableCount == 0)
+                throw Exception("Source must have at least 1 file")
+        }
     }
 
     companion object {
