b2e49719ef99a1d38d7d46f3fa51de5281c887e3
Steven <steventhomson72@yahoo.ca>
2021-09-18 13:04:49 -0400

    updated with new seq column and unique constraint

--- geoflow-core/src/main/kotlin/orm/entities/PipelineTask.kt
+++ geoflow-core/src/main/kotlin/orm/entities/PipelineTask.kt
@@ -3,6 +3,7 @@ package orm.entities
 import org.ktorm.entity.Entity
 
 interface PipelineTask: Entity<PipelineTask> {
+    val pipelineTaskId: Long
     val pipelineId: Long
     val taskId: Long
     val parentTask: Long
--- geoflow-core/src/main/kotlin/orm/tables/PipelineTasks.kt
+++ geoflow-core/src/main/kotlin/orm/tables/PipelineTasks.kt
@@ -6,8 +6,9 @@ import org.ktorm.schema.long
 import orm.entities.PipelineTask
 
 object PipelineTasks: Table<PipelineTask>("pipeline_tasks") {
-    val pipelineId = long("pipeline_id").primaryKey().bindTo { it.pipelineId }
-    val taskId = long("task_id").primaryKey().bindTo { it.taskId }
+    val pipelineTaskId = long("pipeline_task_id").primaryKey().bindTo { it.pipelineTaskId }
+    val pipelineId = long("pipeline_id").bindTo { it.pipelineId }
+    val taskId = long("task_id").bindTo { it.taskId }
     val parentTask = long("parent_task").bindTo { it.parentTask }
     val parentTaskOrder = int("parent_task_order").bindTo { it.parentTaskOrder }
 
@@ -16,7 +17,11 @@ object PipelineTasks: Table<PipelineTask>("pipeline_tasks") {
         (
             pipeline_id bigint NOT NULL,
             task_id bigint NOT NULL,
-            CONSTRAINT pipeline_tasks_pkey PRIMARY KEY (pipeline_id, task_id),
+            parent_task bigint NOT NULL DEFAULT 0,
+            parent_task_order integer NOT NULL,
+            pipeline_task_id bigint NOT NULL DEFAULT nextval('pipeline_tasks_pipeline_task_id_seq'::regclass),
+            CONSTRAINT pipeline_tasks_pkey PRIMARY KEY (pipeline_task_id),
+            CONSTRAINT parent_order_unique UNIQUE (pipeline_id, parent_task, parent_task_order),
             CONSTRAINT pipeline_id FOREIGN KEY (pipeline_id)
                 REFERENCES public.pipelines (pipeline_id) MATCH SIMPLE
                 ON UPDATE CASCADE
