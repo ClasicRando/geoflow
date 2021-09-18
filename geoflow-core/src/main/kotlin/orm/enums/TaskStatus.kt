245754f75e45b1cf9a2bf6fd59122b73b7d5e8be
Steven <steventhomson72@yahoo.ca>
2021-09-18 13:32:35 -0400

    added failed task status

--- geoflow-core/src/main/kotlin/orm/enums/TaskStatus.kt
+++ geoflow-core/src/main/kotlin/orm/enums/TaskStatus.kt
@@ -3,11 +3,12 @@ package orm.enums
 /**
  * Enum type found in DB denoting the task status
  * CREATE TYPE public.task_status AS ENUM
- * ('Waiting', 'Scheduled', 'Running', 'Complete');
+ * ('Waiting', 'Scheduled', 'Running', 'Complete', 'Failed');
  */
 enum class TaskStatus {
     Waiting,
     Scheduled,
     Running,
-    Complete
+    Complete,
+    Failed,
 }
