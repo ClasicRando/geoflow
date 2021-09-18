673ce5d4f83b413df31704d14cbbe4ffe6177d4a
Steven <steventhomson72@yahoo.ca>
2021-09-18 15:36:02 -0400

    added sequence

--- geoflow-core/src/main/kotlin/database/sequences.kt
+++ geoflow-core/src/main/kotlin/database/sequences.kt
@@ -2,8 +2,10 @@ package database
 
 import org.ktorm.database.Database
 import org.ktorm.entity.sequenceOf
+import orm.tables.PipelineRunTasks
 import orm.tables.Roles
 import orm.tables.SourceTables
 
 val Database.roles get() = this.sequenceOf(Roles)
 val Database.sourceTables get() = this.sequenceOf(SourceTables)
+val Database.pipelineRunTasks get() = this.sequenceOf(PipelineRunTasks)
