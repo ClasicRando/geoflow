a03eeafb036496f1f6fe08c57efdce207ca7423b
Steven <steventhomson72@yahoo.ca>
2021-09-18 00:23:49 -0400

    created new enum type for db field

---
+++ geoflow-core/src/main/kotlin/orm/enums/MergeType.kt
@@ -0,0 +1,12 @@
+package orm.enums
+
+/**
+ * Enum type found in DB denoting the type of merge of source files
+ * CREATE TYPE public.merge_type AS ENUM
+ * ('None', 'Exclusive', 'Intersect');
+ */
+enum class MergeType {
+    None,
+    Exclusive,
+    Intersect
+}
