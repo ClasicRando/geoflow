e0e4fb4ee2051f189a7cbe91aca5b3b1833ddf11
Steven <steventhomson72@yahoo.ca>
2021-09-19 01:29:49 -0400

    added code to empty task. Downloads missing files with urls

--- geoflow-core/src/main/kotlin/tasks/DownloadMissingFiles.kt
+++ geoflow-core/src/main/kotlin/tasks/DownloadMissingFiles.kt
@@ -1,5 +1,15 @@
 package tasks
 
+import database.DatabaseConnection
+import database.sourceTables
+import org.ktorm.dsl.eq
+import org.ktorm.dsl.inList
+import org.ktorm.dsl.isNotNull
+import org.ktorm.entity.filter
+import org.ktorm.entity.forEach
+import orm.tables.PipelineRuns
+import web.FileDownloader
+
 class DownloadMissingFiles(pipelineRunTaskId: Long): SystemTask(pipelineRunTaskId) {
 
     companion object {
@@ -8,6 +18,26 @@ class DownloadMissingFiles(pipelineRunTaskId: Long): SystemTask(pipelineRunTaskI
     override val taskId: Long = 4
 
     override suspend fun run() {
-
+        if (task.taskMessage == null)
+            throw IllegalArgumentException("Task message must contain missing filenames")
+        val pipelineRun = PipelineRuns.getRun(task.runId) ?: throw Exception("Run cannot be null")
+        val outputFolder = pipelineRun.dataSource.filesLocation
+        val filenames = task.taskMessage!!
+            .trim('[', ']')
+            .split(",")
+            .map { it.trim('\'') }
+        DatabaseConnection
+            .database
+            .sourceTables
+            .filter { it.runId eq task.runId }
+            .filter { it.fileName.inList(filenames) }
+            .filter { it.url.isNotNull() }
+            .forEach { sourceTable ->
+                FileDownloader(
+                    url = sourceTable.url,
+                    outputPath = outputFolder,
+                    filename = sourceTable.fileName
+                ).request()
+            }
     }
 }
