2e3083eb664546f546fc04b0b77e8912c1d7307e
Steven <steventhomson72@yahoo.ca>
2021-09-25 18:34:27 -0400

    added new modal template for data display

--- geoflow-server/src/main/kotlin/html/bootstrap.kt
+++ geoflow-server/src/main/kotlin/html/bootstrap.kt
@@ -17,20 +17,47 @@ fun FlowContent.basicModal(modalId: String, headerText: String, bodyMessage: Str
                         id = "staticBackdropLabel"
                         +headerText
                     }
-                    button(classes = "close") {
-                        type = ButtonType.button
-                        attributes["data-dismiss"] = "modal"
-                        attributes["aria-label"] = "Close"
-                        span {
-                            attributes["aria-hidden"] = "true"
-                            +"&times;"
-                        }
-                    }
                 }
                 div(classes = "modal-body") {
                     p {
                         +bodyMessage
                     }
+                }
+                div(classes = "modal-footer") {
+                    button(classes = "btn btn-secondary") {
+                        type = ButtonType.button
+                        attributes["data-dismiss"] = "modal"
+                        +"Close"
+                    }
+                    button(classes = "btn btn-secondary") {
+                        type = ButtonType.button
+                        onClick = "pickup()"
+                        +"OK"
+                    }
+                }
+            }
+        }
+    }
+}
+
+fun FlowContent.dataDisplayModal(modalId: String, headerText: String) {
+    div(classes = "modal fade") {
+        id = modalId
+        attributes["data-backdrop"] = "static"
+        attributes["data-keyboard"] = "false"
+        attributes["tabindex"] = "-1"
+        attributes["aria-labelledby"] = "staticBackdropLabel"
+        attributes["aria-hidden"] = "true"
+        div(classes = "modal-dialog modal-dialog-centered") {
+            div(classes = "modal-content") {
+                div(classes = "modal-header") {
+                    h5(classes = "modal-title") {
+                        id = "staticBackdropLabel"
+                        +headerText
+                    }
+                }
+                div(classes = "modal-body") {
+                    id = "modalBody"
                 }
                 div(classes = "modal-footer") {
                     button(classes = "btn btn-secondary") {
