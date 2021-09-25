33714485d2886730e2f35432efba6add0eedd13e
Steven <steventhomson72@yahoo.ca>
2021-09-25 00:29:21 -0400

    added js and modal to pickup ready run

--- geoflow-server/src/main/kotlin/html/PipelineStatus.kt
+++ geoflow-server/src/main/kotlin/html/PipelineStatus.kt
@@ -1,11 +1,11 @@
 package html
 
-import kotlinx.html.script
-import kotlinx.html.unsafe
+import kotlinx.html.*
 import orm.tables.PipelineRuns
 import orm.tables.WorkflowOperations
 
 class PipelineStatus(workflowCode: String): BasePage() {
+    private val modalId = "selectReadyRun"
     init {
         setContent {
             basicTable(
@@ -14,12 +14,36 @@ class PipelineStatus(workflowCode: String): BasePage() {
                 "/api/pipeline-runs?code=$workflowCode",
                 PipelineRuns.tableDisplayFields
             )
+            basicModal(
+                modalId,
+                "Select Run",
+                "Pickup this run to collect?"
+            )
         }
         setScript {
             postObject
             script {
                 unsafe {
-                    raw("$('#runs').on('click-row.bs.table', (e, row, element, field) => { post(row) });")
+                    raw("""
+                        var localRow = {};
+                        function pickup() {
+                            $('#selectReadyRun').modal('toggle');
+                            localRow['pickup'] = 'true';
+                            post(localRow);
+                        }
+                        function handleRowClick(row) {
+                            localRow = row;
+                            const url = new URL(window.location.href);
+                            const urlParams = new URLSearchParams(url.search);
+                            const code = urlParams.get('code');
+                            if (row[`${'$'}{code}_user`] === '') {
+                                $('#$modalId').modal('toggle');
+                            } else {
+                                post(row);
+                            }
+                        }
+                        $('#runs').on('click-row.bs.table', (e, row, element, field) => { handleRowClick(row) });
+                    """.trimIndent())
                 }
             }
         }
