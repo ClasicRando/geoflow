98c9146be6b83f60a79bb5b21fd933d8733b120d
Steven <steventhomson72@yahoo.ca>
2021-09-26 20:41:18 -0400

    consolidated task info and task reset into modal

--- geoflow-server/src/main/kotlin/html/PipelineTasks.kt
+++ geoflow-server/src/main/kotlin/html/PipelineTasks.kt
@@ -54,25 +54,55 @@ class PipelineTasks(runId: Long): BasePage() {
                                 'ID'
                             );
                         }
-                        function handleRowClick(row) {
-                            let ${'$'}modalBody = $('#modalBody');
-                            ${'$'}modalBody.empty();
-                            for (const [key, value] of Object.entries(row)) {
-                                const div = document.createElement('div');
-                                const label = document.createElement('label');
-                                label['for'] = key.replace(/\s+/g, '_');
-                                label.innerHTML = titleCase(key.replace(/_+/g, ' '));
-                                div.appendChild(label);
-                                const textValue = document.createElement('p');
-                                textValue.id = key.replace(/\s+/g, '_');
-                                textValue.innerHTML = value === '' ? ' ' : value;
-                                textValue.classList.add('border', 'rounded', 'p-3');
-                                div.appendChild(textValue);
-                                ${'$'}modalBody.append(div);
+                        function showDataDisplayModal(action, data) {
+                            let options = ${'$'}('#$taskTableId').bootstrapTable('getOptions');
+                            if (options.autoRefreshStatus === false) {
+                                showMessageBox('Error', 'Please turn on auto refresh to select tasks');
+                                return;
                             }
-                            $('#$taskDataModalId').modal('toggle');
+                            let ${'$'}modalBody = ${'$'}('#modalBody');
+                            ${'$'}modalBody.empty();
+                            const div = document.createElement('div');
+                            switch(action) {
+                                case 'choice':
+                                    const btnInfo = document.createElement('button');
+                                    const btnReset = document.createElement('button');
+                                    btnInfo.innerHTML = 'Task Info';
+                                    btnReset.innerHTML = 'Rest Task';
+                                    btnInfo.classList.add('btn','btn-secondary', 'mx-2');
+                                    btnReset.classList.add('btn','btn-secondary', 'mx-2');
+                                    btnInfo.onclick = () => {showDataDisplayModal('info', data);};
+                                    btnReset.onclick = () => {showDataDisplayModal('reset', data);};
+                                    div.appendChild(btnInfo);
+                                    div.appendChild(btnReset);
+                                    ${'$'}modalBody.append(div);
+                                    $('#$taskDataModalId').modal('show');
+                                    break;
+                                case 'reset':
+                                    const params = new URLSearchParams(window.location.href.replace(/^[^?]+/g, ''));
+                                    const prTaskId = data.pipeline_run_task_id;
+                                    postValue(`/api/reset-task?runId=${'$'}{params.get('runId')}&prTaskId=${'$'}{prTaskId}`);
+                                    $('#$taskDataModalId').modal('hide');
+                                    break;
+                                case 'info':
+                                    for (const [key, value] of Object.entries(data)) {
+                                        const label = document.createElement('label');
+                                        label['for'] = key.replace(/\s+/g, '_');
+                                        label.innerHTML = titleCase(key.replace(/_+/g, ' '));
+                                        div.appendChild(label);
+                                        const textValue = document.createElement('p');
+                                        textValue.id = key.replace(/\s+/g, '_');
+                                        textValue.innerHTML = value === '' ? ' ' : value;
+                                        textValue.classList.add('border', 'rounded', 'p-3');
+                                        div.appendChild(textValue);
+                                    }
+                                    ${'$'}modalBody.append(div);
+                                    break;
+                            }
                         }
-                        $('#$taskTableId').on('click-row.bs.table', (e, row, element, field) => { handleRowClick(row) });
+                        $('#$taskTableId').on('click-row.bs.table', (e, row, element, field) => {
+                            showDataDisplayModal('choice', row);
+                        });
                     """.trimIndent())
                 }
             }
