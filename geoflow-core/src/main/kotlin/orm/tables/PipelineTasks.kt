05a9d247ad9df30db7ee75e7f115db0b14fda8f6
Steven <steventhomson72@yahoo.ca>
2021-10-10 22:58:17 -0400

    added missing sequence create statement

--- geoflow-core/src/main/kotlin/orm/tables/PipelineTasks.kt
+++ geoflow-core/src/main/kotlin/orm/tables/PipelineTasks.kt
@@ -31,4 +31,13 @@ object PipelineTasks: DbTable<PipelineTask>("pipeline_tasks") {
             OIDS = FALSE
         );
     """.trimIndent()
+
+    val createSequence = """
+        CREATE SEQUENCE IF NOT EXISTS public.pipeline_tasks_pipeline_task_id_seq
+            INCREMENT 1
+            START 1
+            MINVALUE 1
+            MAXVALUE 9223372036854775807
+            CACHE 1;
+    """.trimIndent()
 }
