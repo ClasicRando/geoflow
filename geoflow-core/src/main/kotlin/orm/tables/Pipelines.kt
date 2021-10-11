de0cbbe22cc17c8ab797c98e884bc6a59e004a2a
Steven <steventhomson72@yahoo.ca>
2021-10-10 22:58:02 -0400

    updated create statement to include missing foreign key line

--- geoflow-core/src/main/kotlin/orm/tables/Pipelines.kt
+++ geoflow-core/src/main/kotlin/orm/tables/Pipelines.kt
@@ -15,7 +15,8 @@ object Pipelines: DbTable<Pipeline>("pipelines") {
             pipeline_id bigint NOT NULL DEFAULT nextval('pipelines_pipeline_id_seq'::regclass),
             name text COLLATE pg_catalog."default" NOT NULL,
             workflow_operation text COLLATE pg_catalog."default" NOT NULL,
-            CONSTRAINT pipelines_pkey PRIMARY KEY (pipeline_id)
+            CONSTRAINT pipelines_pkey PRIMARY KEY (pipeline_id),
+            CONSTRAINT workflow_operations FOREIGN KEY (workflow_operation)
                 REFERENCES public.workflow_operations (code) MATCH SIMPLE
                 ON UPDATE CASCADE
                 ON DELETE SET NULL
