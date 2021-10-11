4bfac4b3e0c68d11ddbc49e7c08773baa7a2af3c
Steven <steventhomson72@yahoo.ca>
2021-10-10 22:59:09 -0400

    added properties to aid in table creation order

--- geoflow-core/src/main/kotlin/orm/tables/DbTable.kt
+++ geoflow-core/src/main/kotlin/orm/tables/DbTable.kt
@@ -5,4 +5,16 @@ import org.ktorm.schema.Table
 
 abstract class DbTable<E: Entity<E>>(tableName: String): Table<E>(tableName) {
     abstract val createStatement: String
+    val hasForeignKey: Boolean
+        get() = createStatement.contains("FOREIGN KEY")
+    val referencedTables: Set<String>
+        get() = if (hasForeignKey) {
+            "REFERENCES public\\.(\\S+) "
+                .toRegex()
+                .findAll(createStatement)
+                .map { it.groupValues[1] }
+                .toSet()
+        } else {
+            setOf()
+        }
 }
