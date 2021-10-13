df8faa5d4d80db5eb4e60e057b57d877113d302c
Steven <steventhomson72@yahoo.ca>
2021-10-13 19:59:14 -0400

    update database classes to reflect new jdbc driver

--- geoflow-core/src/main/kotlin/database/ConnectionProperties.kt
+++ geoflow-core/src/main/kotlin/database/ConnectionProperties.kt
@@ -3,4 +3,11 @@ package database
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class ConnectionProperties(val className: String, val url: String, val username: String, val password: String)
+data class ConnectionProperties(
+    val className: String,
+    val url: String,
+    val username: String,
+    val password: String,
+    val serverName: String,
+    val dbName: String,
+)
