557f782da3bd80f22ebcb3967caba741ea977212
Steven <steventhomson72@yahoo.ca>
2021-10-15 01:42:26 -0400

    added utility function for updating linked db entity

--- geoflow-core/src/main/kotlin/tasks/PipelineTask.kt
+++ geoflow-core/src/main/kotlin/tasks/PipelineTask.kt
@@ -5,6 +5,12 @@ import orm.tables.PipelineRunTasks
 
 abstract class PipelineTask(val pipelineRunTaskId: Long) {
 
+    protected fun updateTask(action: PipelineRunTask.() -> Unit) {
+        with(task) {
+            action()
+            flushChanges()
+        }
+    }
     protected val task: PipelineRunTask by lazy { PipelineRunTasks.getRecord(pipelineRunTaskId) }
     abstract val taskId: Long
     abstract suspend fun runTask(): Boolean
