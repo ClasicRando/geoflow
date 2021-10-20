9717d4060eaabd29c78b3e10e789cf77129ac619
Steven <steventhomson72@yahoo.ca>
2021-10-19 22:58:35 -0400

    added require functions to help with validation

--- geoflow-server/src/main/kotlin/Exceptions.kt
+++ geoflow-server/src/main/kotlin/Exceptions.kt
@@ -1,1 +1,19 @@
+import kotlin.reflect.full.createType
+
 class UnauthorizedRouteAccessException(val route: String): Exception()
+
+fun require(value: Boolean, throwable: () -> Throwable) {
+    if (!value) {
+        throwable()
+    }
+}
+
+@JvmName("requireGeneric")
+inline fun <reified T: Throwable> require(value: Boolean, message: () -> String) {
+    if (!value) {
+        val constructor = T::class.constructors
+            .firstOrNull { it.parameters.size == 1 && it.parameters[0].type == String::class.createType() }
+            ?: throw IllegalArgumentException("Defined Throwable does not have a single String parameter constructor")
+        throw constructor.call(message())
+    }
+}
