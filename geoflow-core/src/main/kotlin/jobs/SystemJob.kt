5f0aed34aff9d8f4dc6dcc6dcb516d8b00e9c221
Steven <steventhomson72@yahoo.ca>
2021-10-24 14:37:25 -0400

    added documentation

--- geoflow-core/src/main/kotlin/jobs/SystemJob.kt
+++ geoflow-core/src/main/kotlin/jobs/SystemJob.kt
@@ -1,7 +1,16 @@
 package jobs
 
 import it.justwrote.kjob.Job
+import tasks.SystemTask
 
+/**
+ * Generic job for kjob to run a [SystemTask]. Requires a [runId], [pipelineRunTaskId] and [taskClassName] to find the
+ * [SystemTask] subclass and instantiate the class, as well as a [runNext] flag to denote if the user asked to keep
+ * running tasks after the first one is completed. Subsequent tasks are only run when next task is a [SystemTask] and
+ * the current task finished successfully
+ *
+ * No execution code is included here, but it can be found in Main.kt of the geoflow-worker module.
+ */
 object SystemJob: Job("system-job") {
     val runId = long("runId")
     val pipelineRunTaskId = long("pipelineRunTaskId")
