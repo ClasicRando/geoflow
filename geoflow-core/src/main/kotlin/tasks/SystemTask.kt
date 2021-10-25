5b3652d99555f370ef1dc9f981f8439fc41fb239
Steven <steventhomson72@yahoo.ca>
2021-10-24 21:49:01 -0400

    added documentation and logging statement

--- geoflow-core/src/main/kotlin/tasks/SystemTask.kt
+++ geoflow-core/src/main/kotlin/tasks/SystemTask.kt
@@ -6,9 +6,15 @@ import orm.enums.TaskStatus
 import orm.tables.PipelineRunTasks
 import java.time.Instant
 
+/**
+ * Base System task. These tasks are instantiated in the worker application to perform operations.
+ *
+ * Adds some extra utility functions and a generic run function for performing operation custom to task
+ */
 abstract class SystemTask(pipelineRunTaskId: Long): PipelineTask(pipelineRunTaskId) {
 
     protected val logger = KotlinLogging.logger {}
+    /** Buildable message to be applied to the table record after runTask completion */
     private val message = StringBuilder()
 
     abstract suspend fun run()
@@ -17,10 +23,20 @@ abstract class SystemTask(pipelineRunTaskId: Long): PipelineTask(pipelineRunTask
         message.append(text)
     }
 
+    /** Clears current message text and appends the provided [text] */
     protected fun setMessage(text: String) {
         message.clear().append(text)
     }
 
+    /**
+     * Implementation of the abstract runTask function from parent class. Prepares task then runs task operation.
+     *
+     * Steps are as follows:
+     * 1. Updates task to running state
+     * 2. Locks record for future update while task is running
+     * 3. Runs task operation, catching any throwable
+     * 4. If no errors are thrown, return Success with message if not blank. If error throw, return error with throwable
+     */
     override suspend fun runTask(): TaskResult {
         updateTask {
             set(it.taskStart, Instant.now())
@@ -33,6 +49,7 @@ abstract class SystemTask(pipelineRunTaskId: Long): PipelineTask(pipelineRunTask
                 run()
                 TaskResult.Success(message.toString().takeIf { it.isNotBlank() })
             }.getOrElse { t ->
+                logger.error("Error for $pipelineRunTaskId: ${t.message ?: "No message provided. See record"}")
                 TaskResult.Error(t)
             }
         }
