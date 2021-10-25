33b1487a24f709c616fc2543b6db7bc216105e8c
Steven <steventhomson72@yahoo.ca>
2021-10-24 22:19:36 -0400

    added documentation

--- geoflow-core/src/main/kotlin/tasks/FirstPipelineDetected.kt
+++ geoflow-core/src/main/kotlin/tasks/FirstPipelineDetected.kt
@@ -1,5 +1,8 @@
 package tasks
 
+/**
+ * User task to notify the user that this data source has never been loaded before
+ */
 class FirstPipelineDetected(pipelineRunTaskId: Long): UserTask(pipelineRunTaskId) {
     override val taskId: Long = 10
 
