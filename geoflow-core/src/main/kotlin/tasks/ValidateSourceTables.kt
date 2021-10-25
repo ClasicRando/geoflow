1b6e53ae4e0f8d02233b5d5e02b6da56b88108d4
Steven <steventhomson72@yahoo.ca>
2021-10-24 22:23:23 -0400

    added documentation and expanded validation

--- geoflow-core/src/main/kotlin/tasks/ValidateSourceTables.kt
+++ geoflow-core/src/main/kotlin/tasks/ValidateSourceTables.kt
@@ -8,6 +8,16 @@ import org.ktorm.entity.filter
 import org.ktorm.entity.forEach
 import orm.enums.LoaderType
 
+/**
+ * System task to validate the source table options are correct.
+ *
+ * Fixes any source files that are easily repaired with the [UpdateFiles] procedure then proceeds to validate that each
+ * file that needs to contain a sub table has a non-null and non-blank sub table value
+ *
+ * Future Changes
+ * --------------
+ * - add more file validation steps
+ */
 class ValidateSourceTables(pipelineRunTaskId: Long): SystemTask(pipelineRunTaskId) {
 
     override val taskId: Long = 8
@@ -20,7 +30,7 @@ class ValidateSourceTables(pipelineRunTaskId: Long): SystemTask(pipelineRunTaskI
             .filter { it.runId eq task.runId }
             .forEach { sourceTable->
                 if (sourceTable.loaderType == LoaderType.Excel || sourceTable.loaderType == LoaderType.MDB) {
-                    if (sourceTable.subTable == null)
+                    if (sourceTable.subTable.isNullOrBlank())
                         throw Exception(
                             "${sourceTable.loaderType.name} file ${sourceTable.fileName} cannot have a null sub table"
                         )
