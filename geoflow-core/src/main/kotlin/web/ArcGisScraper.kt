adb5f32745796423b85221cc7f03331a041b2263
Steven <steventhomson72@yahoo.ca>
2021-10-27 23:51:58 -0400

    added documentation

--- geoflow-core/src/main/kotlin/web/ArcGisScraper.kt
+++ geoflow-core/src/main/kotlin/web/ArcGisScraper.kt
@@ -5,19 +5,23 @@ import com.univocity.parsers.csv.CsvWriter
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.buffer
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.withContext
 import java.io.File
 import kotlin.coroutines.coroutineContext
 
-@Suppress("BlockingMethodInNonBlockingContext")
+/**
+ * Convenience method to wrap ArcGIS REST service scraping. Uses a [url] to collect [metadata][ArcGisServiceMetadata],
+ * fetch scraping queries, and consolidate temp files into a single file written to the [outputPath] specified
+ */
 suspend fun scrapeArcGisService(url: String, outputPath: String) {
     with(ArcGisServiceMetadata.fromUrl(url)) {
         val file = File(outputPath, "$name.csv")
-        file.createNewFile()
         with(CsvWriter(file, csvSettings)) {
             writeHeaders()
             fetchQueries()
                 .buffer()
+                .flowOn(Dispatchers.IO)
                 .collect { tempFile ->
                     CsvParser(csvParserSettings)
                         .iterate(tempFile.bufferedReader())
