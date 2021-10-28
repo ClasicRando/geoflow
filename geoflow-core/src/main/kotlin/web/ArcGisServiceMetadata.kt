1a90dd70d07f0cb525ddfe8ffff167f0d0c499ab
Steven <steventhomson72@yahoo.ca>
2021-10-27 23:51:51 -0400

    added documentation, fixed a geometry fetching bug and consolidated some code

--- geoflow-core/src/main/kotlin/web/ArcGisServiceMetadata.kt
+++ geoflow-core/src/main/kotlin/web/ArcGisServiceMetadata.kt
@@ -17,10 +17,19 @@ import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.*
+import mu.KotlinLogging
 import java.io.File
 import kotlin.jvm.Throws
 import kotlin.math.ceil
 
+/**
+ * Class to store and work with metadata related to ArcGIS REST services. Main usage is to scrape all features but that
+ * is not required and can just be used to collect or report service metadata.
+ *
+ * Instances are created using a companion object suspend function ([fromUrl]) since instantiation requires an HTTP
+ * request. Scraping should be done using the scrapeArcGisService function but if you need access to the queries used to
+ * fetch the service's data, you can use the [queries] property.
+ */
 class ArcGisServiceMetadata private constructor(
     val url: String,
     val name: String,
@@ -35,33 +44,29 @@ class ArcGisServiceMetadata private constructor(
     val maxMinOid: Pair<Int, Int>,
     val incrementalOid: Boolean,
 ) {
+    /** Scrape chunk size. Uses service max record count but caps that value to 10000 */
     private val scrapeCount = maxRecordCount.takeIf { maxRecordCount <= 10000 } ?: 10000
+    /** Number of queries required to scrape all features using the OID field */
     private val oidQueryCount = ceil((maxMinOid.first - maxMinOid.second + 1) / scrapeCount.toFloat()).toInt()
+    /** Number of queries required to scape all features using pagination */
     private val paginationQueryCount = ceil(sourceCount / scrapeCount.toFloat()).toInt()
+    /** True if service is a table. No geometry type is specified in query url */
     private val isTable = serverType == "TABLE"
+    /** Geometry specification portion of query url */
     private val geoText = if (isTable) "" else "&geometryType=${geoType}&outSR=4269"
+    private val logger = KotlinLogging.logger {}
+    /** Settings used to write temp csv files for each query. Specifies what headers are used */
     val csvSettings = CsvWriterSettings().apply {
         setHeaders(*fields.toTypedArray())
     }
+    /** Settings used to parse temp csv files for each query. Specifies that the csv files have a header */
     val csvParserSettings = CsvParserSettings().apply {
         isHeaderExtractionEnabled = true
     }
-
-    private fun paginationQuery(queryNumber: Int): String {
-        return """
-            /query?where=1+%3D+1&resultOffset=${queryNumber * scrapeCount}
-            &resultRecordCount=${scrapeCount}${geoText}&outFields=*&f=json
-        """.trimIndent().replace("\n", "")
-    }
-
-    private fun oidQuery(minOid: Int): String {
-        return """
-            /query?where=${oidField}+>%3D+${minOid}+and+${oidField}+<%3D+${minOid + scrapeCount - 1}
-            ${geoText}&outFields=*&f=json
-        """.trimIndent().replace("\n", "")
-    }
-
-    fun fetchQueries() = when {
+    /**
+     * Sequence of query url strings for each scraping method. Returns an empty sequence when no method available
+     */
+    val queries = when {
         pagination -> (0 until paginationQueryCount)
             .asSequence()
             .map { url + paginationQuery(it) }
@@ -69,19 +74,48 @@ class ArcGisServiceMetadata private constructor(
             .asSequence()
             .map { url + oidQuery(maxMinOid.second + (it * scrapeCount)) }
         else -> sequenceOf()
-    }.asFlow().map {
-        fetchQuery(it)
     }
 
+    /** Constructs a query url using a pagination offset of the [queryNumber] multiplied by the features per query */
+    private fun paginationQuery(queryNumber: Int): String {
+        return """
+            /query?where=1+%3D+1&resultOffset=${queryNumber * scrapeCount}
+            &resultRecordCount=${scrapeCount}${geoText}&outFields=*&f=json
+        """.trimIndent().replace("\n", "")
+    }
+
+    /**
+     * Constructs a query url using a range of OID values. Starts at [minOid] with an upper bound defined by the
+     * [scrapeCount]
+     */
+    private fun oidQuery(minOid: Int): String {
+        return """
+            /query?where=${oidField}+>%3D+${minOid}+and+${oidField}+<%3D+${minOid + scrapeCount - 1}
+            ${geoText}&outFields=*&f=json
+        """.trimIndent().replace("\n", "")
+    }
+
+    /** Cold flow of query responses mapped to temp csv files */
+    fun fetchQueries() = queries.asFlow().map { query ->
+        logger.info("Fetching query: $query")
+        fetchQuery(query)
+    }
+
+    /**
+     * Transforms a service [feature] JSON object into a [Map] of nullable Strings that can be used to write a CSV file
+     */
     @OptIn(ExperimentalSerializationApi::class)
-    private fun handleRecord(feature: JsonObject): Map<String, Any?> {
+    private fun handleRecord(feature: JsonObject): Map<String, String?> {
         val record = Json.decodeFromJsonElement<Map<String, JsonPrimitive>>(
             feature["attributes"] ?: JsonObject(mapOf())
         ).mapValues { (_, value) -> value.contentOrNull }
         val geoFields = when (geoType) {
-            "esriGeometryPoint" -> feature["geometry"]
-                ?.jsonObject
-                ?.mapValues { (_, value) -> value.jsonPrimitive.contentOrNull } ?: mapOf()
+            "esriGeometryPoint" -> {
+                feature["geometry"]?.jsonObject
+                    ?.entries
+                    ?.associate { (key, value) -> key.uppercase() to value.jsonPrimitive.contentOrNull }
+                    ?: mapOf()
+            }
             "esriGeometryMultipoint" -> mapOf(
                 "POINTS" to Json.encodeToString(feature["geometry"]?.jsonObject?.get("points")?.jsonArray).trim()
             )
@@ -93,6 +127,13 @@ class ArcGisServiceMetadata private constructor(
         return record + geoFields
     }
 
+    /**
+     * Uses [url] query to fetch a JSON response and transform each feature to a writeable csv record. Returns a
+     * reference to a temp file that can be used to build the entire services features.
+     *
+     * Makes an HTTP request and retries until it gets a valid JSON response, or it exceeds the max number of tries.
+     * Once a valid response is received, a temp file is created and the features are written to the temp file.
+     */
     @OptIn(ExperimentalSerializationApi::class)
     @Suppress("BlockingMethodInNonBlockingContext")
     private suspend fun fetchQuery(url: String): File {
@@ -124,19 +165,16 @@ class ArcGisServiceMetadata private constructor(
                 }
             }
         }
-        return withContext(Dispatchers.IO) {
-            File.createTempFile("temp", ".csv").also { file ->
-                file.deleteOnExit()
-                val headerMapping = fields.associateWith { it }
-                val writer = CsvWriter(file.bufferedWriter(), csvSettings)
-                writer.writeHeaders()
-                jsonResponse["features"]?.jsonArray?.let { array ->
-                    val records = array.map { handleRecord(it.jsonObject) }
-                    val rowData = fields.associateWith { field -> records.map { it[field] }.toTypedArray() }
-                    writer.writeObjectRows(headerMapping, rowData)
+        return File.createTempFile("temp", ".csv").also { file ->
+            file.deleteOnExit()
+            val writer = CsvWriter(file.bufferedWriter(), csvSettings)
+            writer.writeHeaders()
+            jsonResponse["features"]?.jsonArray?.let { array ->
+                for (record in array) {
+                    writer.writeRow(handleRecord(record.jsonObject))
                 }
-                writer.close()
             }
+            writer.close()
         }
     }
 
@@ -144,8 +182,10 @@ class ArcGisServiceMetadata private constructor(
         private const val countQuery = "/query?where=1%3D1&returnCountOnly=true&f=json"
         private const val oidQuery = "/query?where=1%3D1&returnIdsOnly=true&f=json"
         private const val fieldQuery = "?f=json"
+        /** Delay between query attempts if an erroneous response is received */
         private const val delayMilli = 10000L
         private const val maxTries = 10
+        /** Query extension to find the max and min OID values for a service. Only used when stats are supported */
         private fun maxMinQuery(oidField: String) = """
            /query?outStatistics=%5B%0D%0A+%7B%0D%0A++++"statisticType"%3A+"max"%2C%0D%0A++++"onStatisticField
            "%3A+"$oidField"%2C+++++%0D%0A++++"outStatisticFieldName"%3A+"MAX_VALUE"%0D%0A
@@ -154,6 +194,12 @@ class ArcGisServiceMetadata private constructor(
            ++%7D%0D%0A%5D&f=json'
         """.trimIndent().replace("\n", "")
 
+        /**
+         * Returns [metadata][ArcGisServiceMetadata] from the provided [url] that points to an ArcGIS REST service.
+         *
+         * Only method to obtain a class instance. Since the class requires data obtained from an HTTP request, the
+         * class creation method has to be suspendable to avoid the class constructor blocking the current thread.
+         */
         @Throws(TypeCastException::class)
         suspend fun fromUrl(url: String): ArcGisServiceMetadata {
             return HttpClient(CIO){
@@ -202,20 +248,19 @@ class ArcGisServiceMetadata private constructor(
                     ?.jsonPrimitive?.content ?: ""
                 val maxMinOid = when {
                     !pagination && stats && oidField.isNotEmpty() -> {
-                        with(client.get<JsonObject>(url + maxMinQuery(oidField))) {
-                            this["features"]
-                                ?.jsonArray
-                                ?.get(0)
-                                ?.jsonObject
-                                ?.get("attributes")
-                                ?.jsonObject
-                                ?.let { attributes ->
-                                    Pair(
-                                        attributes["MAX_VALUE"]?.jsonPrimitive?.int ?: -1,
-                                        attributes["MIN_VALUE"]?.jsonPrimitive?.int ?: -1
-                                    )
-                                } ?: Pair(-1, -1)
-                        }
+                        val response = client.get<JsonObject>(url + maxMinQuery(oidField))
+                        response["features"]
+                            ?.jsonArray
+                            ?.get(0)
+                            ?.jsonObject
+                            ?.get("attributes")
+                            ?.jsonObject
+                            ?.let { attributes ->
+                                Pair(
+                                    attributes["MAX_VALUE"]?.jsonPrimitive?.int ?: -1,
+                                    attributes["MIN_VALUE"]?.jsonPrimitive?.int ?: -1
+                                )
+                            } ?: Pair(-1, -1)
                     }
                     !pagination && oidField.isNotEmpty() -> {
                         with(client.get<JsonObject>(url + oidQuery)) {
