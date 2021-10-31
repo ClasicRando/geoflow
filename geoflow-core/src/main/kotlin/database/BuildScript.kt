762fec3468113b5ac6b1a409bfb4bbccab9f85d1
Steven <steventhomson72@yahoo.ca>
2021-10-31 18:43:04 -0400

    added documentation

--- geoflow-core/src/main/kotlin/database/BuildScript.kt
+++ geoflow-core/src/main/kotlin/database/BuildScript.kt
@@ -10,6 +10,7 @@ import org.reflections.scanners.Scanners.*
 import orm.tables.*
 import java.sql.Connection
 
+/** Container for an enum translation to a PostgreSQL enum creation */
 data class PostgresEnumType(val name: String, val constantValues: List<String>) {
     val postgresName = name.replace("[A-Z]".toRegex()) { "_" + it.value.lowercase() }.trimStart('_')
     val create = """
@@ -23,12 +24,18 @@ data class PostgresEnumType(val name: String, val constantValues: List<String>) 
     """.trimIndent()
 }
 
+/** Lazy list of table build interfaces. Uses reflection to get all interface classes that are used to build tables */
 private val tableInterfaces by lazy {
     Reflections("orm.tables")
         .get(SubTypes.of(TableBuildRequirement::class.java))
         .map { className -> ClassLoader.getSystemClassLoader().loadClass(className) }
 }
 
+/**
+ * Lazy sequence of enums needed for database operations.
+ *
+ * Uses reflection to find all Enum classes and maps the class to a PostgresEnumType.
+ */
 private val enums by lazy {
     Reflections("orm.enums")
         .get(SubTypes.of(Enum::class.java).asClass<Enum<*>>())
@@ -38,6 +45,7 @@ private val enums by lazy {
         }
 }
 
+/** Lazy list of tables declared in the 'orm.tables' package. List items are the Object instances themselves. */
 private val tables by lazy {
     Reflections("orm.tables")
         .get(SubTypes.of(DbTable::class.java).asClass<DbTable<*>>())
@@ -48,6 +56,10 @@ private val tables by lazy {
         .toList()
 }
 
+/**
+ * Lazy sequence of procedures declared in the 'database.procedures' package. List items are the Object instances
+ * themselves.
+ */
 private val procedures by lazy {
     Reflections("database.procedures")
         .get(SubTypes.of(SqlProcedure::class.java).asClass<SqlProcedure>())
@@ -57,6 +69,10 @@ private val procedures by lazy {
         .map { kClass -> kClass.objectInstance!! as SqlProcedure }
 }
 
+/**
+ * Lazy sequence of table functions declared in the 'database.functions' package. List items are the Object instances
+ * themselves.
+ */
 private val tableFunctions by lazy {
     Reflections("database.functions")
         .get(SubTypes.of(PlPgSqlTableFunction::class.java).asClass<PlPgSqlTableFunction>())
@@ -68,6 +84,18 @@ private val tableFunctions by lazy {
 
 private val logger = KotlinLogging.logger {}
 
+/**
+ * Extension function to create a given [table] instance within the current Connection. Multiple steps might be required
+ * depending upon the complexity of the table definition. Steps may include:
+ * 1. If the primary key is serial (ie table extends interface [SequentialPrimaryKey]), find the PK field and the
+ * sequence name in the CREATE TABLE statement then execute the generated CREATE SEQUENCE statement.
+ * 2. Execute the CREATE TABLE statement stored in [createStatement][DbTable.createStatement] property.
+ * 3. If the table has triggers (ie table extends interface [Triggers]), loop through list of [Trigger] data classes
+ * to create the trigger function then run the CREATE TRIGGER statement.
+ * 4. If the table had initial data to load (ie table extends interface [DefaultData]), get the resource's
+ * [InputStream][java.io.InputStream] to COPY the file to the current table. When the load is done, set the sequence
+ * value to the current max value in the table if the table has a sequential primary key.
+ */
 private fun Connection.createTable(table: DbTable<*>) {
     logger.info("Starting ${table.tableName}")
     require(!checkTableExists(table.tableName)) { "${table.tableName} already exists" }
@@ -140,6 +168,17 @@ private fun Connection.createTable(table: DbTable<*>) {
     }
 }
 
+/**
+ * Recursive function that creates all tables initially passed, accounting for foreign key dependencies.
+ *
+ * Initial state of the database is no tables created and a full list of [tables] to create. The first iteration works
+ * with all tables that do not have foreign key constraints since they can be created easily without complex
+ * dependencies. Once those tables are created, the function calls itself with a new list of tables to create (the
+ * passed list with tables created in this iteration removed) and a running [set of tables][createdTables] already
+ * created. All iterations after this point, create tables that have foreign keys and all the dependencies can be found
+ * in the set of already created tables, and each time a table is created, it is added to running set of tables created.
+ * The process exits the recursion when the passed list of tables to create is empty.
+ */
 private fun Connection.createTables(
     tables: List<DbTable<*>>,
     createdTables: Set<String> = setOf()
@@ -158,6 +197,9 @@ private fun Connection.createTables(
     createTables(tables.minus(tablesToCreate), createdTables.union(tablesToCreate.map { it.tableName }))
 }
 
+/**
+ * Uses the database connection to create all required objects, tables, procedures, and table functions for application.
+ */
 fun buildDatabase() {
     try {
         DatabaseConnection.database.useConnection { connection ->
