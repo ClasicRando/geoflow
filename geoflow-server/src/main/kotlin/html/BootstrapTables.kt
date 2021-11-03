1a1db61d55175403131a451dbf1a50ff13d3d963
Steven <steventhomson72@yahoo.ca>
2021-11-02 20:18:08 -0400

    added documentation and removed auto refresh table

--- geoflow-server/src/main/kotlin/html/BootstrapTables.kt
+++ geoflow-server/src/main/kotlin/html/BootstrapTables.kt
@@ -2,6 +2,7 @@ package html
 
 import kotlinx.html.*
 
+/** Utility function to convert a JSON key to a display field name */
 fun getFieldTable(field: String): String = field
     .replace("_", " ")
     .fold(StringBuilder()) { acc, c ->
@@ -9,6 +10,11 @@ fun getFieldTable(field: String): String = field
     }
     .toString()
 
+/**
+ * Container template for buttons found in a table. Uses properties to construct JSON entry.
+ *
+ * See [buttons](https://bootstrap-table.com/docs/api/table-options/#buttons) option for more details.
+ */
 data class TableButton(val name: String, val text: String, val icon: String, val event: String, val title: String) {
     val button = """
         $name: {
@@ -22,8 +28,17 @@ data class TableButton(val name: String, val text: String, val icon: String, val
     """.trimIndent()
 }
 
+/** Container for table header buttons. Provides a [name] and [html code][html] for the html list entry */
 data class HeaderButton(val name: String, val html: UL.() -> Unit)
 
+/**
+ * Constructs a basic [BootstrapTable](https://bootstrap-table.com) that fetches data from the desired [url][dataUrl],
+ * displays the desired [fields] using the keys as reference to JSON data's keys. The [fields] parameter also allows the
+ * user to add [column options](https://bootstrap-table.com/docs/api/column-options/) as a map of option to value.
+ * Optional parameters include [table buttons](https://bootstrap-table.com/docs/api/table-options/#buttons),
+ * [header buttons](https://bootstrap-table.com/docs/api/table-options/#toolbar), sort function (name of javascript
+ * function to call), a flag denoting is the rows are [clickable][clickableRows], and a [subscriber] url.
+ */
 fun FlowContent.basicTable(
     tableId: String,
     dataUrl: String,
@@ -72,66 +87,6 @@ fun FlowContent.basicTable(
                         if (clickableRows) {
                             attributes["data-cell-style"] = "clickableTd"
                         }
-                        options.filter { it.key != "name" }.forEach { (key, value) ->
-                            attributes["data-$key"] = value
-                        }
-                        text(options["name"] ?: getFieldTable(field))
-                    }
-                }
-            }
-        }
-        if (tableButtons.isNotEmpty()) {
-            script {
-                unsafe {
-                    raw("""
-                        function ${tableId}buttons() {
-                            return {
-                                ${tableButtons.joinToString { it.button }}
-                            }
-                        }
-                    """.trimIndent())
-                }
-            }
-        }
-    }
-}
-
-fun FlowContent.autoRefreshTable(
-    tableId: String,
-    dataUrl: String,
-    fields: Map<String, Map<String, String>>,
-    tableButtons: List<TableButton> = listOf(),
-    headerButtons: List<HeaderButton> = listOf(),
-) {
-    if (headerButtons.isNotEmpty()) {
-        ul(classes = "header-button-list") {
-            id = "toolbar"
-            for (headerButton in headerButtons) {
-                headerButton.html(this)
-            }
-        }
-    }
-    table {
-        id = tableId
-        attributes["data-toggle"] = "table"
-        if (headerButtons.isNotEmpty()) {
-            attributes["data-toolbar"] = "#toolbar"
-        }
-        attributes["data-url"] = dataUrl
-        attributes["data-auto-refresh"] = "true"
-        attributes["data-auto-refresh-interval"] = "1"
-        attributes["data-auto-refresh-status"] = "true"
-        attributes["data-classes"] = "table table-bordered"
-        attributes["data-thead-classes"] = "thead-dark"
-        attributes["data-search"] = "true"
-        if (tableButtons.isNotEmpty()) {
-            attributes["data-buttons"] = "${tableId}buttons"
-        }
-        thead {
-            tr {
-                fields.forEach { (field, options) ->
-                    th {
-                        attributes["data-field"] = field
                         options.filter { it.key != "name" }.forEach { (key, value) ->
                             attributes["data-$key"] = value
                         }
