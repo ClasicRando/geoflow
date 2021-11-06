69315b9424461e9b100b02c35bc909ff44bb73ab
Steven <steventhomson72@yahoo.ca>
2021-11-06 12:55:34 -0400

    added function to get a data class from a ResultSet row

--- geoflow-core/src/main/kotlin/utils.kt
+++ geoflow-core/src/main/kotlin/utils.kt
@@ -1,8 +1,13 @@
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.*
 import tasks.UserTask
+import java.sql.ResultSet
 import java.time.Instant
 import java.time.LocalDate
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
+import kotlin.reflect.full.findAnnotation
+import kotlin.reflect.full.hasAnnotation
 
 fun formatLocalDateDefault(date: LocalDate): String = date.format(DateTimeFormatter.ISO_LOCAL_DATE)
 
@@ -26,5 +31,24 @@ inline fun <T> requireNotEmpty(collection: Collection<T>, lazyMessage: () -> Any
     if (collection.isEmpty()) {
         val message = lazyMessage()
         throw IllegalArgumentException(message.toString())
+    }
+}
+
+inline fun <reified T> ResultSet.rowToDataClass(): T {
+    require(!isClosed) { "ResultSet is closed" }
+    require(!isAfterLast) { "ResultSet has no more rows to return" }
+    require(T::class.isData) { "Type provided is not a data class" }
+    val constructor = T::class.constructors.first()
+    val row = 0.until(metaData.columnCount).map { getObject(it + 1) }.toTypedArray()
+    return if (T::class.hasAnnotation<Serializable>()) {
+        require(row.size == constructor.parameters.size - 2) {
+            "Row size must match number of required constructor parameters"
+        }
+        constructor.call(Int.MAX_VALUE, *row, null)
+    } else {
+        require(row.size == constructor.parameters.size) {
+            "Row size must match number of required constructor parameters"
+        }
+        constructor.call(*row)
     }
 }
