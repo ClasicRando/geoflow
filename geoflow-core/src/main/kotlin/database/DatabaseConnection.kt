651aa92d4bb93df71c122cd2c8cd711c62efaa6e
Steven <steventhomson72@yahoo.ca>
2021-11-06 12:56:07 -0400

    added query and execute utility functions

--- geoflow-core/src/main/kotlin/database/DatabaseConnection.kt
+++ geoflow-core/src/main/kotlin/database/DatabaseConnection.kt
@@ -1,11 +1,20 @@
 package database
 
+import com.github.michaelbull.jdbc.context.CoroutineDataSource
+import com.github.michaelbull.jdbc.context.connection
+import com.github.michaelbull.jdbc.transaction
+import com.github.michaelbull.jdbc.withConnection
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 import org.apache.commons.dbcp2.BasicDataSource
 import org.ktorm.database.Database
+import rowToDataClass
 import java.io.File
+import java.sql.Connection
 
 /**
  * Singleton to hold the primary database connection as a [DataSource][javax.sql.DataSource] and anything else linked to
@@ -22,5 +31,65 @@ object DatabaseConnection {
             password = props.password
         }
     }
-    val database = Database.connect(dataSource)
+    val database by lazy { Database.connect(dataSource) }
+    val scope = CoroutineScope(Dispatchers.IO + CoroutineDataSource(dataSource))
+
+    suspend inline fun <reified T> submitQuery(
+        sql: String,
+        parameters: List<Any?> = listOf(),
+    ): List<T> {
+        return queryConnection { connection ->
+            connection.prepareStatement(sql).apply {
+                for (parameter in parameters.withIndex()) {
+                    setObject(parameter.index + 1, parameter.value)
+                }
+            }.use { statement ->
+                statement.executeQuery().use { rs ->
+                    generateSequence {
+                        if (rs.next()) rs.rowToDataClass<T>() else null
+                    }.toList()
+                }
+            }
+        }
+    }
+
+    suspend inline fun <T> queryConnection(
+        crossinline func: suspend CoroutineScope.(Connection) -> List<T>,
+    ): List<T> {
+        return withContext(scope.coroutineContext) {
+            withConnection {
+                func(coroutineContext.connection)
+            }
+        }
+    }
+
+    suspend inline fun <T> queryConnectionSingle(
+        crossinline func: suspend CoroutineScope.(Connection) -> T,
+    ): T {
+        return withContext(scope.coroutineContext) {
+            withConnection {
+                func(coroutineContext.connection)
+            }
+        }
+    }
+
+    suspend inline fun execute(
+        crossinline func: suspend CoroutineScope.(Connection) -> Unit
+    ) {
+        withContext(scope.coroutineContext) {
+            withConnection {
+                func(coroutineContext.connection)
+            }
+        }
+    }
+
+    suspend inline fun useTransaction(
+        crossinline func: suspend CoroutineScope.(Connection) -> Unit
+    ) {
+        return withContext(scope.coroutineContext) {
+            transaction {
+                func(coroutineContext.connection)
+            }
+        }
+    }
 }
