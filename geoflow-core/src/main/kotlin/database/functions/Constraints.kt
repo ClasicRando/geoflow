f308f22c2e9879d34971cf0702dfe7f78e140bac
Steven <steventhomson72@yahoo.ca>
2021-11-14 12:31:53 -0500

    updated to list of function create statements

--- geoflow-core/src/main/kotlin/database/functions/Constraints.kt
+++ geoflow-core/src/main/kotlin/database/functions/Constraints.kt
@@ -2,18 +2,50 @@ package database.functions
 
 object Constraints {
 
-    /** Function used to verify a text field is not empty or containing only whitespace. Returns true for null values */
-    val checkBlankOrNull = """
-        CREATE OR REPLACE FUNCTION public.check_blank_or_empty(
-        	"textInput" text)
+    val functions = listOf(
+        /**
+         * Returns true if the value passed is null or text containing characters other than whitespace
+         */
+        """
+        CREATE OR REPLACE FUNCTION public.check_not_blank_or_empty(
+            text)
             RETURNS boolean
             LANGUAGE 'plpgsql'
             COST 100
             VOLATILE PARALLEL UNSAFE
-        AS         ${'$'}BODY${'$'}
-        begin
-        return coalesce($1,'x') ~ '^\s*$';
-        end;
+        AS ${'$'}BODY${'$'}
+        BEGIN
+            RETURN COALESCE($1,'x') !~ '^\s*$';
+        END;
+        ${'$'}BODY${'$'};
+        """.trimIndent(),
+        /**
+         * Returns true if all values passed in the array are null or text containing characters other than whitespace.
+         * Returns false if the array passed is empty.
+         */
+        """
+        CREATE OR REPLACE FUNCTION public.check_array_not_blank_or_empty(
+        	text[])
+            RETURNS boolean
+            LANGUAGE 'plpgsql'
+            COST 100
+            VOLATILE PARALLEL UNSAFE
+        AS ${'$'}BODY${'$'}
+        DECLARE
+        	val text;
+        BEGIN
+        	IF array_length($1, 1) = 0 THEN
+        		RETURN false;
+        	END IF;
+        	FOREACH val IN ARRAY $1
+        	LOOP
+        		IF COALESCE(val,'x') ~ '^\s*$' THEN
+        			RETURN false;
+        		END IF;
+        	END LOOP;
+        	RETURN true;
+        END;
         ${'$'}BODY${'$'};
     """.trimIndent()
+    )
 }
