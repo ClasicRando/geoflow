4b9c4f4a9a8c5f81c4dc4158bb93e1b4aa0789f9
Steven <steventhomson72@yahoo.ca>
2021-11-15 00:34:35 -0500

    created test for tasks

---
+++ geoflow-core/src/test/kotlin/tasks/TaskChecks.kt
@@ -0,0 +1,55 @@
+package tasks
+
+import database.Database
+import database.tables.PipelineRunTasks
+import database.tables.Tasks
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import java.sql.Connection
+import kotlin.reflect.full.createType
+import kotlin.test.assertFalse
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class TaskChecks {
+
+    private val connectionType = Connection::class.createType()
+    private val prTaskType = PipelineRunTasks.PipelineRunTask::class.createType()
+
+    @Test
+    fun checkTaskParameters() {
+        val messages = mutableListOf<String>()
+        var isFail = false
+        for ((taskId, info) in tasks) {
+            val message = checkTask(taskId, info)
+            if (message != null) {
+                isFail = true
+                messages += message
+            }
+        }
+        assertFalse(isFail, messages.joinToString(separator = "\n"))
+    }
+
+    private fun checkTask(taskId: Long, info: TaskInfo): String? {
+        runBlocking {
+            Database.runWithConnection {
+                Tasks.getRecord(it, taskId)
+            }
+        } ?: return "TaskId = $taskId could not be found in the `tasks` table"
+        return when (info) {
+            is TaskInfo.SystemTaskInfo -> {
+                when {
+                    info.function.parameters.size != 2 -> "TaskId = $taskId, system tasks require 2 parameters"
+                    info.function.parameters[0].type != connectionType ->
+                        "TaskId = $taskId, system tasks require the first parameter to be a Connection"
+                    info.function.parameters[1].type != prTaskType ->
+                        "TaskId = $taskId, system tasks require the second parameter to be a PipelineRunTask type"
+                    else -> null
+                }
+            }
+            is TaskInfo.UserTaskInfo -> {
+                null
+            }
+        }
+    }
+}
