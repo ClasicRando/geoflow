b50efb8ddcd9bd5083db9e0ddd1c122f91510c38
Steven <steventhomson72@yahoo.ca>
2021-11-17 23:06:53 -0500

    added custom require utilities and added documentation

--- geoflow-core/src/main/kotlin/Utils.kt
+++ geoflow-core/src/main/kotlin/Utils.kt
@@ -11,15 +11,40 @@ fun formatInstantDateTime(timestamp: Instant?) = timestamp
     ?.atZone(ZoneId.systemDefault())
     ?.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME) ?: ""
 
+/**
+ * Checks if the provided [collection] is empty and if so, throws an [IllegalStateException] with the lazy message
+ */
 inline fun <T> requireNotEmpty(collection: Collection<T>, lazyMessage: () -> Any) {
     if (collection.isEmpty()) {
         val message = lazyMessage()
-        throw IllegalArgumentException(message.toString())
+        throw IllegalStateException(message.toString())
+    }
+}
+/**
+ * Checks if the provided [collection] is not empty and if so, throws an [IllegalStateException] with the lazy message
+ */
+inline fun <T> requireEmpty(collection: Collection<T>, lazyMessage: () -> Any) {
+    if (collection.isNotEmpty()) {
+        val message = lazyMessage()
+        throw IllegalStateException(message.toString())
     }
 }
 
+/**
+ * Returns a new array that applies a transformation to the receiver
+ */
 inline fun <reified T, reified R> Array<T>.mapToArray(transform: (T) -> R): Array<R> {
     return Array(this.size) { index ->
         transform(this[index])
+    }
+}
+
+/**
+ * Checks the required [value] and if the value is false, an [IllegalStateException] is thrown with the lazy message.
+ */
+inline fun requireState(value: Boolean, lazyMessage: () -> String) {
+    if (!value) {
+        val message = lazyMessage()
+        throw IllegalStateException(message)
     }
 }
