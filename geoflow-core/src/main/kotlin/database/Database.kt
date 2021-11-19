5d980372f3f4aef6470bc6f541589b7eefaf108b
Steven <steventhomson72@yahoo.ca>
2021-11-18 23:02:39 -0500

    documented function

--- geoflow-core/src/main/kotlin/database/Database.kt
+++ geoflow-core/src/main/kotlin/database/Database.kt
@@ -28,8 +28,13 @@ object Database {
     }
 
     private val logger = KotlinLogging.logger {}
+    /** Coroutine Scope used to run coroutines on the IO [Dispatchers] with the [dataSource] as a context element */
     val scope = CoroutineScope(Dispatchers.IO + CoroutineDataSource(dataSource))
 
+    /**
+     * Runs the provided lambda with the current coroutineContext's connection or a new connection from the [dataSource]
+     * if a connection is not currently open in the context
+     */
     inline fun <reified T> CoroutineScope.useConnection(block: CoroutineScope.(Connection) -> T): T {
         val connection = if (coroutineContext.hasOpenConnection()) {
             coroutineContext.connection
@@ -41,10 +46,18 @@ object Database {
         }
     }
 
+    /**
+     * Runs the provided lambda in a blocking manner with a connection from the [dataSource]. Frees the connection when
+     * done
+     */
     fun <T> runWithConnectionBlocking(func: (Connection) -> T): T {
         return dataSource.connection.use(func)
     }
 
+    /**
+     * Runs the provided lambda in a non-blocking context with a connection for the current context of the database
+     * [scope]. Frees the connection when done
+     */
     suspend inline fun <reified T> runWithConnection(crossinline func: (Connection) -> T): T {
         return withContext(scope.coroutineContext) {
             useConnection {
@@ -53,7 +66,10 @@ object Database {
         }
     }
 
-
+    /**
+     * Runs the provided suspending lambda in a non-blocking context with a connection for the current context of the
+     * database [scope]. Frees the connection when done
+     */
     suspend inline fun <reified T> runWithConnectionAsync(crossinline func: suspend (Connection) -> T): T {
         return withContext(scope.coroutineContext) {
             useConnection {
@@ -62,6 +78,10 @@ object Database {
         }
     }
 
+    /**
+     * Runs the provided suspending lambda in a non-blocking context within a transaction for the current context of the
+     * database [scope]. Commits the transaction when done
+     */
     suspend inline fun <T> useTransaction(
         crossinline func: suspend CoroutineScope.(Connection) -> T
     ): T {
@@ -72,6 +92,7 @@ object Database {
         }
     }
 
+    /** Checks to see if the current [CoroutineContext] has an open connection to use */
     fun CoroutineContext.hasOpenConnection(): Boolean {
         val connection = get(CoroutineConnection)?.connection
         return connection != null && !connection.isClosedCatching()
