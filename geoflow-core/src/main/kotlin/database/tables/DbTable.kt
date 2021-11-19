08ade11c9fa824685a41377d68a5e64b55b23454
Steven <steventhomson72@yahoo.ca>
2021-11-19 17:10:40 -0500

    added documentation and updated foreign key checks

--- geoflow-core/src/main/kotlin/database/tables/DbTable.kt
+++ geoflow-core/src/main/kotlin/database/tables/DbTable.kt
@@ -1,12 +1,19 @@
 package database.tables
 
+/**
+ * Base definition of a database table. Contains some constraints and default implementations of properties used by
+ * the BuildScript.
+ *
+ * @param tableName name of the table in the database
+ */
 abstract class DbTable(val tableName: String) {
     /**
      * SQL create statement for the table
      */
     abstract val createStatement: String
+    /** Checks to see if the CREATE statement contains the REFERENCE keyword (denotes a FOREIGN KEY constraint) */
     val hasForeignKey: Boolean
-        get() = createStatement.contains("FOREIGN KEY")
+        get() = createStatement.contains("REFERENCES")
     /**
      * Property showing the distinct table names that this table references
      *
@@ -14,7 +21,7 @@ abstract class DbTable(val tableName: String) {
      */
     val referencedTables: Set<String>
         get() = if (hasForeignKey) {
-            "REFERENCES public\\.(\\S+) "
+            "REFERENCES (public\\.)?(\\S+) "
                 .toRegex()
                 .findAll(createStatement)
                 .map { it.groupValues[1] }
