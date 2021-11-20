e750de47b32b877f91f915d77d927a9c00bd03a7
Steven <steventhomson72@yahoo.ca>
2021-11-20 01:25:24 -0500

    expanded documentation

--- geoflow-core/src/main/kotlin/tasks/TaskResult.kt
+++ geoflow-core/src/main/kotlin/tasks/TaskResult.kt
@@ -4,10 +4,19 @@ package tasks
  * Defines class that can be returned as a result of a pipeline task. Sealed to contain possible result types
  */
 sealed interface TaskResult {
-    /** Task run was successful. Provides an optional message if the task wants to report warning or detail */
+    /**
+     * Task run was successful. Provides an optional message if the task wants to report warning or detail
+     *
+     * @param message optional message for successful task completion
+     */
     data class Success(val message: String? = null) : TaskResult
-    /** Task run has an error. Provides the throwable instance to get message and stack trace for tracking */
+    /**
+     * Task run has an error. Provides the throwable instance to get message and stack trace for tracking
+     *
+     * @param throwable exception thrown when the task encounters an error
+     */
     data class Error(val throwable: Throwable) : TaskResult {
-        val message = "ERROR in Task: ${throwable.message ?: throwable.toString()}"
+        /** Basic message from the throwable */
+        val message: String = "ERROR in Task: ${throwable.message ?: throwable.toString()}"
     }
 }
