dccdc29fd400c1ebf0ab141d85b0f783ecb62150
Steven <steventhomson72@yahoo.ca>
2021-11-20 01:24:16 -0500

    added documentation

--- geoflow-core/src/main/kotlin/tasks/SystemTask.kt
+++ geoflow-core/src/main/kotlin/tasks/SystemTask.kt
@@ -1,4 +1,10 @@
 package tasks
 
+/**
+ * Function annotation to mark the function as a system task that is runnable by the worker. On worker setup, reflection
+ * is used to find all annotated system tasks to register them for later usage
+ *
+ * @param taskId unique ID for the task to be found for running later
+ */
 @Target(AnnotationTarget.FUNCTION)
 annotation class SystemTask(val taskId: Long)
--- geoflow-core/src/main/kotlin/tasks/UserTask.kt
+++ geoflow-core/src/main/kotlin/tasks/UserTask.kt
@@ -1,4 +1,7 @@
 package tasks
 
+/**
+ * Field annotation to denote that a property is the constant value of a task
+ */
 @Target(AnnotationTarget.FIELD)
 annotation class UserTask
