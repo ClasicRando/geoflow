b7b3ed08732be40ae9c4d2e7fc84623d9155eecd
Steven <steventhomson72@yahoo.ca>
2021-11-20 20:56:58 -0500

    added new exception and require check

--- geoflow-server/src/main/kotlin/Exceptions.kt
+++ geoflow-server/src/main/kotlin/Exceptions.kt
@@ -1,15 +1,33 @@
 @file:Suppress("MatchingDeclarationName")
 
+import kotlin.reflect.full.primaryConstructor
+
 /** Exception thrown when a user requests a [route] that they are not authorized to access. */
 class UnauthorizedRouteAccessException(
     /** route that denied access to the current user */
     val route: String,
 ): Throwable()
 
+/**
+ * Exception thrown when the [ApplicationCall][io.ktor.application.ApplicationCall] does not have a valid
+ * [UserSession][auth.UserSession]
+ */
+class NoValidSessionException: Throwable()
+
 /** Require contract that throws any [Throwable] the user wants by providing a lambda that returns any Throwable. */
 fun require(value: Boolean, block: () -> Throwable) {
     if (!value) {
         val error = block()
         throw error
+    }
+}
+
+/** */
+inline fun <reified T: Throwable> requireOrThrow(value: Boolean) {
+    if (!value) {
+        val constructor = T::class.primaryConstructor
+        requireNotNull(constructor) { "Throwable must have constructor" }
+        requireEmpty(constructor.parameters) { "Throwable's constructor must accept 0 parameters" }
+        throw constructor.call()
     }
 }
