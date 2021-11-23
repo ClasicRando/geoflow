c42d081dcad2c543b4de22b38752532e1e69a8e6
Steven <steventhomson72@yahoo.ca>
2021-11-23 03:01:46 -0500

    updated api errors definitions to better specify error types

--- geoflow-server/src/main/kotlin/api/ApiUtils.kt
+++ geoflow-server/src/main/kotlin/api/ApiUtils.kt
@@ -1,5 +1,8 @@
 package api
 
+import kotlinx.serialization.SerializationException
+import java.sql.SQLException
+
 /** alias describing API error information as array of objects */
 typealias ApiErrors = List<Map<String, String?>>
 
@@ -9,8 +12,19 @@ typealias ApiErrors = List<Map<String, String?>>
  */
 fun throwableToResponseErrors(t: Throwable): ApiErrors {
     return t.suppressedExceptions.plus(t).map {
+        val errorName = when (it) {
+            is SQLException -> "sql_error"
+            is SerializationException -> "json_decode_error"
+            is IllegalArgumentException -> "argument_error"
+            is IllegalStateException -> "internal_state_error"
+            else -> {
+                it::class.simpleName?.replace("([A-Z])([a-z])".toRegex()) { match ->
+                    "${match.groupValues[1].lowercase()}_${match.groupValues[2]}"
+                }?.lowercase()
+            }
+        }
         mapOf(
-            "class_name" to it::class.simpleName,
+            "error_name" to errorName,
             "message" to it.message,
         )
     }
