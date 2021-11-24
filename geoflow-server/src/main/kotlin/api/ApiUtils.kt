52ce7b001bbe1956896be120d590d3b64243613e
Steven <steventhomson72@yahoo.ca>
2021-11-24 00:22:41 -0500

    added for error codes and api response templates

--- geoflow-server/src/main/kotlin/api/ApiUtils.kt
+++ geoflow-server/src/main/kotlin/api/ApiUtils.kt
@@ -1,5 +1,14 @@
 package api
 
+import database.NoRecordAffected
+import database.NoRecordFound
+import io.ktor.application.ApplicationCall
+import io.ktor.application.MissingApplicationFeatureException
+import io.ktor.application.call
+import io.ktor.application.log
+import io.ktor.request.path
+import io.ktor.response.respond
+import io.ktor.util.pipeline.PipelineContext
 import kotlinx.serialization.SerializationException
 import java.sql.SQLException
 
@@ -28,4 +37,58 @@ fun throwableToResponseErrors(t: Throwable): ApiErrors {
             "message" to it.message,
         )
     }
+}
+
+/** */
+@Suppress("MagicNumber")
+fun errorCodeFromThrowable(t: Throwable): Int {
+    return when (t) {
+        is MissingApplicationFeatureException, is IllegalArgumentException, is SerializationException -> 406
+        is NoRecordFound, is NoRecordAffected -> 404
+        else -> 500
+    }
+}
+
+/** Utility function that summarizes api response for a single object into some parameters and a getter lambda */
+suspend inline fun <reified T> PipelineContext<Unit, ApplicationCall>.apiResponseSingle(
+    responseObject: String,
+    errorMessage: String,
+    func: PipelineContext<Unit, ApplicationCall>.() -> T
+) {
+    val response = runCatching {
+        ApiResponse.SuccessSingle(
+            responseObject = responseObject,
+            payload = func()
+        )
+    }.getOrElse { t ->
+        call.application.log.error(call.request.path(), t)
+        ApiResponse.Error(
+            code = errorCodeFromThrowable(t),
+            message = errorMessage,
+            errors = throwableToResponseErrors(t)
+        )
+    }
+    call.respond(response)
+}
+
+/** Utility function that summarizes api response for multiple objects into some parameters and a getter lambda */
+suspend inline fun <reified T> PipelineContext<Unit, ApplicationCall>.apiResponseMulti(
+    responseObject: String,
+    errorMessage: String,
+    func: PipelineContext<Unit, ApplicationCall>.() -> List<T>
+) {
+    val response = runCatching {
+        ApiResponse.SuccessMulti(
+            responseObject = responseObject,
+            payload = func()
+        )
+    }.getOrElse { t ->
+        call.application.log.error(call.request.path(), t)
+        ApiResponse.Error(
+            code = errorCodeFromThrowable(t),
+            message = errorMessage,
+            errors = throwableToResponseErrors(t)
+        )
+    }
+    call.respond(response)
 }
