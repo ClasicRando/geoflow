83b69fbdb2742418750974c8e8e61b952329b859
Steven <steventhomson72@yahoo.ca>
2021-11-25 00:44:11 -0500

    removed "/jobs/tasks" api V1 endpoint

--- geoflow-server/src/main/kotlin/Routes.kt
+++ geoflow-server/src/main/kotlin/Routes.kt
@@ -1,7 +1,5 @@
 @file:Suppress("TooManyFunctions")
 
-import api.ApiResponse
-import api.throwableToResponseErrors
 import auth.UserSession
 import auth.requireUserRole
 import database.Database
@@ -40,8 +38,6 @@ import io.ktor.sessions.sessions
 import io.ktor.sessions.set
 import io.ktor.util.getOrFail
 import jobs.SystemJob
-import kotlinx.coroutines.flow.toList
-import mongo.MongoDb
 
 /** Open login to anyone and respond with login page. */
 fun Route.loginGet() {
@@ -398,27 +394,6 @@ fun Route.api() {
                     mapOf("error" to "Failed to create new user. ${t.message}")
                 }
                 call.respond(response)
-            }
-        }
-        route("/jobs") {
-            route("/tasks") {
-                get {
-                    val response = runCatching {
-                        val request = call.receive<MongoDb.ApiRequest>()
-                        ApiResponse.SuccessMulti(
-                            responseObject = "scheduled_job",
-                            payload = MongoDb.getTasks(request).toList(),
-                        )
-                    }.getOrElse { t ->
-                        call.application.log.info("GET /api/jobs/tasks", t)
-                        ApiResponse.Error(
-                            code = 404,
-                            message = "Failed to get kjob tasks",
-                            errors = throwableToResponseErrors(t)
-                        )
-                    }
-                    call.respond(response)
-                }
             }
         }
     }
