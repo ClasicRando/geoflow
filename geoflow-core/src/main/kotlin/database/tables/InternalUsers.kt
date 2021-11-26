9f26682cb14600091256daf6f4c434c7d45c960d
Steven <steventhomson72@yahoo.ca>
2021-11-25 23:21:11 -0500

    updated documentation for RequestUser

--- geoflow-core/src/main/kotlin/database/tables/InternalUsers.kt
+++ geoflow-core/src/main/kotlin/database/tables/InternalUsers.kt
@@ -123,22 +123,22 @@ object InternalUsers : DbTable("internal_users"), ApiExposed {
         ) ?: throw IllegalArgumentException("User cannot be found")
     }
 
-    /** API request to */
+    /** API request to [InternalUsers] table */
     @Serializable
     data class RequestUser(
-        /** */
+        /** user_oid field for request. Can be null for create requests */
         @SerialName("user_oid")
         val userOid: Long? = null,
-        /** */
+        /** name field for request */
         @SerialName("fullName")
         val name: String,
-        /** */
+        /** username field for request */
         @SerialName("username")
         val username: String,
-        /** */
+        /** roles field for request */
         @SerialName("roles")
         val roles: List<String>,
-        /** */
+        /** password field for request. Only non-null when create request. Never send a hashed password in response */
         @SerialName("password")
         val password: String? = null,
     )
