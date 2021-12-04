ef83271e3ae39ce498bc375c317466c7470f687c
Steven <steventhomson72@yahoo.ca>
2021-12-04 00:45:45 -0500

    Added spinner to submit button to show progress

--- geoflow-server/src/main/resources/javascript/login.js
+++ geoflow-server/src/main/resources/javascript/login.js
@@ -16,7 +16,13 @@ $(document).ready(() => {
         validClass: 'validInput',
         submitHandler: () => {},
     });
-    $('#submit').click(async () => {
+    $('#submit').click(async (e) => {
+        const $button = $(e.target)
+        $button.prop('disabled', true);
+        e.target.innerHTML = `
+            <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+            Loading...
+        `;
         const user = {
             username: $('#username').val(),
             password: $('#password').val(),
@@ -28,13 +34,16 @@ $(document).ready(() => {
                 if ('success' in json) {
                     redirect('/index');
                 } else {
+                    $button.empty();
                     $('#message').text(json.error);
                 }
             } else {
+                $button.empty();
                 console.log(await response.text());
                 $('#message').text('Response content not json. See console for details');
             }
         } else {
+            $button.empty();
             console.log(await response.text());
             $('#message').text('Invalid Response');
         }
