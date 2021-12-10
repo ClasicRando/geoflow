9f42cbbad4338d5a98e123c91fe0c83f0eb92e27
Steven <steventhomson72@yahoo.ca>
2021-12-10 16:16:24 -0500

    Fixed tasks to properly check user and system tasks

--- geoflow-core/src/test/kotlin/tasks/TaskChecks.kt
+++ geoflow-core/src/test/kotlin/tasks/TaskChecks.kt
@@ -1,8 +1,8 @@
 package tasks
 
 import me.geoflow.core.database.Database
-import me.geoflow.core.database.tables.PipelineRunTasks
 import me.geoflow.core.database.tables.Tasks
+import me.geoflow.core.database.tables.records.PipelineRunTask
 import me.geoflow.core.tasks.SystemTask
 import me.geoflow.core.tasks.UserTask
 import org.junit.jupiter.api.Test
@@ -13,6 +13,7 @@ import org.reflections.util.ConfigurationBuilder
 import java.sql.Connection
 import kotlin.reflect.KParameter
 import kotlin.reflect.KType
+import kotlin.reflect.full.createType
 import kotlin.reflect.full.isSubtypeOf
 import kotlin.reflect.full.withNullability
 import kotlin.reflect.jvm.kotlinFunction
@@ -39,7 +40,7 @@ class TaskChecks {
     private val userTasks: List<UserTaskField> by lazy {
         val config = ConfigurationBuilder()
             .forPackage("me.geoflow.core.tasks")
-            .setScanners(Scanners.MethodsAnnotated)
+            .setScanners(Scanners.FieldsAnnotated)
         Reflections(config)
             .getFieldsAnnotatedWith(UserTask::class.java)
             .map { field ->
@@ -80,20 +81,21 @@ class TaskChecks {
             Tasks.getSystemTasks(it)
         }.associate { it.taskId to it.name }
     }
-    private val connectionType = typeOf<Connection>()
-    private val prTaskType = typeOf<PipelineRunTasks.PipelineRunTask>()
+    private val connectionType = Connection::class.createType()//typeOf<Connection>()
+    private val prTaskType = typeOf<PipelineRunTask>()
     private val longType = typeOf<Long>()
     private val unitType = typeOf<Unit>()
     private val nullableStringType = typeOf<String>().withNullability(true)
 
     @Test
     fun checkCodebaseUserTasks() {
+        assertTrue(userTasks.isNotEmpty(), "Could not find any user tasks")
         val message = userTasks.mapNotNull { checkUserTask(it) }
         assertTrue(message.isEmpty(), message.joinToString(separator = "\n"))
     }
 
     private fun checkUserTask(userTaskField: UserTaskField): String? {
-        if (userTaskField.fieldType.isSubtypeOf(longType)) {
+        if (!userTaskField.fieldType.isSubtypeOf(longType)) {
             return "UserTask annotated fields must be of type `Long`, got ${userTaskField.fieldName}"
         }
         val taskId = userTaskField.fieldValue as Long
@@ -109,6 +111,7 @@ class TaskChecks {
 
     @Test
     fun checkCodebaseSystemTasks() {
+        assertTrue(systemTasks.isNotEmpty(), "Could not find any system tasks")
         val message = systemTasks.mapNotNull { checkSystemTask(it) }
         assertTrue(message.isEmpty(), message.joinToString(separator = "\n"))
     }
@@ -122,10 +125,14 @@ class TaskChecks {
         return when {
             name == null  -> "Could not find codebase taskId = $taskId in the database"
             systemTaskMethod.parameters.size != 2 -> "TaskId = $taskId, system tasks require 2 parameters"
-            systemTaskMethod.parameters[0].type.isSubtypeOf(connectionType) ->
-                "TaskId = $taskId, system tasks require the first parameter to be a Connection"
-            systemTaskMethod.parameters[1].type.isSubtypeOf(prTaskType)->
-                "TaskId = $taskId, system tasks require the second parameter to be a PipelineRunTask type"
+            !systemTaskMethod.parameters[0].type.isSubtypeOf(connectionType) -> {
+                val firstParameter = systemTaskMethod.parameters[0].type
+                "TaskId = $taskId, system tasks require the first parameter to be $connectionType, got $firstParameter"
+            }
+            !systemTaskMethod.parameters[1].type.isSubtypeOf(prTaskType) -> {
+                val secondParameter = systemTaskMethod.parameters[1].type
+                "TaskId = $taskId, system tasks require the second parameter to be $prTaskType, got $secondParameter"
+            }
             else -> null
         }
     }
