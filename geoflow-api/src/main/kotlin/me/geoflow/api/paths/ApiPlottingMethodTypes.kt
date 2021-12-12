fe8276a7bbcfca24f3b25a98e8eb44389f8d9619
Steven <steventhomson72@yahoo.ca>
2021-12-11 22:03:37 -0500

    Created new endpoint for plotting method types

---
+++ geoflow-api/src/main/kotlin/me/geoflow/api/paths/ApiPlottingMethodTypes.kt
@@ -0,0 +1,26 @@
+package me.geoflow.api.paths
+
+import io.ktor.http.HttpMethod
+import io.ktor.routing.Route
+import me.geoflow.api.utils.ApiResponse
+import me.geoflow.api.utils.apiCall
+import me.geoflow.core.database.Database
+import me.geoflow.core.database.tables.PlottingMethodTypes
+
+/** Plotting method types API route */
+object ApiPlottingMethodTypes : ApiPath(path = "/plotting-method-types") {
+
+    override fun Route.registerEndpoints() {
+        getTypes(this)
+    }
+
+    /** Returns a list of all plotting method types available */
+    private fun getTypes(parent: Route) {
+        parent.apiCall(httpMethod = HttpMethod.Get) {
+            val payload = Database.runWithConnection {
+                PlottingMethodTypes.getRecords(it)
+            }
+            ApiResponse.PlottingMethodTypesResponse(payload)
+        }
+    }
+}
