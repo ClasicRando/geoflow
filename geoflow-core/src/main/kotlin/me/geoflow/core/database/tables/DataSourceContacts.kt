d89c9bc48076c0cc2c8b5917d97ad2d3a572d8d0
Steven <steventhomson72@yahoo.ca>
2021-12-13 17:27:59 -0500

    Added crud methods to tables

--- geoflow-core/src/main/kotlin/me/geoflow/core/database/tables/DataSourceContacts.kt
+++ geoflow-core/src/main/kotlin/me/geoflow/core/database/tables/DataSourceContacts.kt
@@ -1,9 +1,16 @@
 package me.geoflow.core.database.tables
 
+import me.geoflow.core.database.errors.NoRecordAffected
+import me.geoflow.core.database.errors.UserMissingRole
+import me.geoflow.core.database.extensions.runReturningFirstOrNull
+import me.geoflow.core.database.extensions.runUpdate
+import me.geoflow.core.database.extensions.submitQuery
+import me.geoflow.core.database.tables.records.DataSourceContact
+import org.postgresql.util.PGobject
+import java.sql.Connection
+
 /**
- * Table used to store contacts of a DataSource
- *
- * This table should never be used by itself but rather referenced in [DataSources] table
+ * Table used to store contacts of a [DataSource][me.geoflow.core.database.tables.records.DataSource]
  */
 object DataSourceContacts: DbTable("data_source_contacts") {
 
@@ -15,7 +22,7 @@ object DataSourceContacts: DbTable("data_source_contacts") {
                 ON UPDATE CASCADE
                 ON DELETE CASCADE,
             name text COLLATE pg_catalog."default" CHECK(check_not_blank_or_empty(name)),
-            email text COLLATE pg_catalog."default" CHECK(check_not_blank_or_empty(email)),
+            email email COLLATE pg_catalog."default",
             website text COLLATE pg_catalog."default" CHECK(check_not_blank_or_empty(website)),
             type text COLLATE pg_catalog."default" CHECK(check_not_blank_or_empty(type)),
             notes text COLLATE pg_catalog."default" CHECK(check_not_blank_or_empty(notes))
@@ -24,4 +31,83 @@ object DataSourceContacts: DbTable("data_source_contacts") {
             OIDS = FALSE
         );
     """.trimIndent()
+
+    /** Returns a list of [DataSourceContact] record for the provided [dsId] */
+    fun getRecords(connection: Connection, dsId: Long): List<DataSourceContact> {
+        return connection.submitQuery(sql = "SELECT * FROM $tableName WHERE ds_id = ?", dsId)
+    }
+
+    /**
+     * Updates the record specified by the contactId in the [contact] object. Will not update ds_id field and ds_id
+     * value in [contact] must match the existing record.
+     *
+     * @throws IllegalArgumentException the [contact] contains a null contact_id field
+     * @throws UserMissingRole the caller does not have the 'collection' role
+     * @throws java.sql.SQLException exception thrown during sql UPDATE
+     * @throws NoRecordAffected the update was not successful so the affected count is 0
+     */
+    fun updateRecord(connection: Connection, userId: Long, contact: DataSourceContact): DataSourceContact {
+        requireNotNull(contact.contactId) { "Updating of a data source contact record requires a non-null contact_id" }
+        InternalUsers.requireRole(connection, userId, "collection")
+        val updateCount = connection.runUpdate(
+            sql = """
+                UPDATE $tableName
+                SET    name = ?,
+                       email = ?,
+                       website = ?,
+                       type = ?,
+                       notes = ?
+                WHERE  contact_id = ?
+                AND    ds_id = ?
+            """.trimIndent(),
+            contact.name?.takeIf { it.isNotBlank() },
+            PGobject().apply {
+                type = "email"
+                value = contact.email
+            },
+            contact.website?.takeIf { it.isNotBlank() },
+            contact.type?.takeIf { it.isNotBlank() },
+            contact.notes?.takeIf { it.isNotBlank() },
+            contact.contactId,
+            contact.dsId,
+        )
+        if (updateCount == 0) {
+            throw NoRecordAffected(
+                tableName,
+                "Update of data source contact was not successful or the contact_id + ds_id does not exist"
+            )
+        }
+        return contact
+    }
+
+    /**
+     * Creates a new data source contact record for the provided [contact] object. Linked to the ds_id provided within
+     * the [contact] record.
+     *
+     * @throws IllegalArgumentException the [contact] contains a non-null contact_id field
+     * @throws UserMissingRole the caller does not have the 'collection' role
+     * @throws java.sql.SQLException exception thrown during sql INSERT
+     * @throws NoRecordAffected the insert was not successful so the new contact_id was not returned
+     */
+    fun createRecord(connection: Connection, userId: Long, contact: DataSourceContact): Long {
+        require(contact.contactId == null) { "ContactId provided must be null to create a record" }
+        InternalUsers.requireRole(connection, userId, "collection")
+        return connection.runReturningFirstOrNull(
+            sql = """
+                INSERT INTO data_source_contacts(name,email,website,type,notes,ds_id)
+                VALUES(?,?,?,?,?,?)
+                RETURNING contact_id
+            """.trimIndent(),
+            contact.name?.takeIf { it.isNotBlank() },
+            PGobject().apply {
+                type = "email"
+                value = contact.email
+            },
+            contact.website?.takeIf { it.isNotBlank() },
+            contact.type?.takeIf { it.isNotBlank() },
+            contact.notes?.takeIf { it.isNotBlank() },
+            contact.dsId,
+        ) ?: throw NoRecordAffected(tableName, "No record inserted into data source contacts")
+    }
+
 }
--- geoflow-core/src/main/kotlin/me/geoflow/core/database/tables/DataSources.kt
+++ geoflow-core/src/main/kotlin/me/geoflow/core/database/tables/DataSources.kt
@@ -1,15 +1,23 @@
 package me.geoflow.core.database.tables
 
+import me.geoflow.core.database.errors.NoRecordAffected
+import me.geoflow.core.database.errors.NoRecordFound
+import me.geoflow.core.database.errors.UserMissingRole
+import me.geoflow.core.database.extensions.queryFirstOrNull
+import me.geoflow.core.database.extensions.runReturningFirstOrNull
+import me.geoflow.core.database.extensions.runUpdate
+import me.geoflow.core.database.extensions.submitQuery
+import me.geoflow.core.database.tables.records.DataSource
+import me.geoflow.core.database.tables.records.DataSourceRequest
+import java.io.File
+import java.sql.Connection
+
 /**
  * Table used to store sources of data. Core backing of how data is grouped and moves through the system from collection
  * to reporting.
  *
  * The records contain data about how the source is moved through the system, treated during reporting and other meta
  * details about the source
- *
- * Future Changes
- * --------------
- * - implement API requirements for users to perform CRUD operations on this table
  */
 object DataSources : DbTable("data_sources") {
 
@@ -52,4 +60,127 @@ object DataSources : DbTable("data_sources") {
             OIDS = FALSE
         );
     """.trimIndent()
+
+    /**
+     * Returns a single [DataSource] record for the provided [dsId]
+     *
+     * @throws NoRecordFound when the dsId cannot be found
+     * @throws java.sql.SQLException when the connection throws an exception
+     */
+    fun getRecord(connection: Connection, dsId: Long): DataSource {
+        return connection.queryFirstOrNull(sql = "${DataSource.sql} WHERE ds_id = ?", dsId)
+            ?: throw NoRecordFound(tableName, "Could not find a data source record for ds_id = $dsId")
+    }
+
+    /**
+     * Returns a list of [DataSource] records
+     *
+     * @throws UserMissingRole when the user does not have the 'ds_create' or 'admin' role
+     * @throws java.sql.SQLException when the connection throws an exception
+     */
+    fun getRecords(connection: Connection, userId: Long): List<DataSource> {
+        InternalUsers.requireRole(connection, userId, "ds_create")
+        return connection.submitQuery(sql = DataSource.sql)
+    }
+
+    /**
+     * Updates the record specified by the dsId in [request] with the contents of the [request].
+     *
+     * Checks to make sure the prov level and prov field correlate and the user has the role required.
+     *
+     * @throws IllegalArgumentException when the source is prov level and the prov is null or blank or the source is not
+     * prov level and the prov is not null or blank
+     * @throws UserMissingRole when the user does not have the 'ds_create' or 'admin' role
+     * @throws java.sql.SQLException when the connection throws an exception
+     */
+    fun updateRecord(connection: Connection, userId: Long, request: DataSourceRequest): DataSource {
+        requireNotNull(request.dsId) { "ds_id provided in the request body cannot be null" }
+        if (request.provLevel && request.prov.isNullOrBlank()) {
+            throw IllegalArgumentException("Specified data source is prov level but the prov field is null or blank")
+        }
+        if (!request.provLevel && !request.prov.isNullOrBlank()) {
+            throw IllegalArgumentException(
+                "Specified data source is not prov level but the prov field is not null or blank"
+            )
+        }
+        InternalUsers.requireRole(connection, userId, "collection")
+        connection.runUpdate(
+            sql = """
+                UPDATE $tableName
+                SET    code = ?,
+                       prov = ?,
+                       description = ?,
+                       files_location = ?,
+                       prov_level = ?,
+                       comments = ?,
+                       assigned_user = ?,
+                       search_radius = ?,
+                       record_warehouse_type = ?,
+                       reporting_type = ?,
+                       collection_pipeline = ?,
+                       load_pipeline = ?,
+                       check_pipeline = ?,
+                       qa_pipeline = ?,
+                       updated_by = ?,
+                       last_updated = timezone('utc'::text, now())
+                WHERE  ds_id = ?
+            """.trimIndent(),
+            request.code,
+            request.prov.takeIf { request.provLevel },
+            request.description,
+            request.filesLocation,
+            request.provLevel,
+            request.comments?.takeIf { it.isNotBlank() },
+            request.assignedUser,
+            request.searchRadius,
+            request.recordWarehouseType,
+            request.reportingType,
+            request.collectionPipeline,
+            request.loadPipeline,
+            request.checkPipeline,
+            request.qaPipeline,
+            userId,
+            request.dsId,
+        )
+        return getRecord(connection, request.dsId)
+    }
+
+    /**
+     * Creates a data source record using the [request] data and returns the newly generated ds_id if successful.
+     *
+     * @throws UserMissingRole the [userId] does not have the 'ds_create' or 'admin' role
+     * @throws IllegalArgumentException the provided files_location or search_radius are not valid
+     * @throws java.sql.SQLException connection throws exception
+     * @throws NoRecordAffected sql statement did not return the newly created ds_id due to an error
+     */
+    fun createRecord(connection: Connection, userId: Long, request: DataSourceRequest): Long {
+        InternalUsers.requireRole(connection, userId, "ds_create")
+        require(File(request.filesLocation).exists()) { "File location provided does not exist" }
+        require(request.searchRadius > 0) { "Search radius value must be greater than zero" }
+        return connection.runReturningFirstOrNull(
+            sql = """
+                INSERT INTO data_sources(code,prov,description,files_location,prov_level,comments,assigned_user,
+                                         search_radius,record_warehouse_type,reporting_type,collection_pipeline,
+                                         load_pipeline,check_pipeline,qa_pipeline,created_by)
+                VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?)
+                RETURNING ds_id
+            """.trimIndent(),
+            request.code,
+            request.prov.takeIf { request.provLevel },
+            request.description,
+            request.filesLocation,
+            request.provLevel,
+            request.comments?.takeIf { it.isNotBlank() },
+            request.assignedUser,
+            request.searchRadius,
+            request.recordWarehouseType,
+            request.reportingType,
+            request.collectionPipeline,
+            request.loadPipeline,
+            request.checkPipeline,
+            request.qaPipeline,
+            userId,
+        ) ?: throw NoRecordAffected(tableName, "No record inserted for ds_id = ${request.dsId}")
+    }
+
 }
