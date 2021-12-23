c21cd4e6a8c617f32fba04a3b2ffe6ca492d8ebd
Steven <steventhomson72@yahoo.ca>
2021-12-23 00:48:27 -0500

    Removed dependencies that are no longer needed

--- geoflow-server/build.gradle.kts
+++ geoflow-server/build.gradle.kts
@@ -6,11 +6,9 @@ plugins {
 
 version = "0.1"
 val kotlinVersion: String by project
-val kjobVersion: String by project
 val ktorVersion: String by project
 val htmlJvmVersion: String by project
 val slf4Version: String by project
-val postgresqlVersion: String by project
 val kotlinLoggingVersion: String by project
 
 dependencies {
@@ -18,10 +16,6 @@ dependencies {
     testImplementation(kotlin("test", kotlinVersion))
     // https://mvnrepository.com/artifact/org.jetbrains.kotlin/kotlin-reflect
     implementation("org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion")
-    // https://mvnrepository.com/artifact/it.justwrote/kjob-core
-    implementation("it.justwrote:kjob-core:$kjobVersion")
-    // https://mvnrepository.com/artifact/it.justwrote/kjob-mongo
-    implementation("it.justwrote:kjob-mongo:$kjobVersion")
     implementation("io.ktor:ktor-server-core:$ktorVersion")
     implementation("io.ktor:ktor-server-cio:$ktorVersion")
     implementation("io.ktor:ktor-html-builder:$ktorVersion")
