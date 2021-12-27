677b65fddaea3dabdaa4e524e2675ec2e25c5302
Steven <steventhomson72@yahoo.ca>
2021-12-27 15:15:16 -0500

    Updated page to allow switching between workflow operations

--- geoflow-server/src/main/kotlin/me/geoflow/web/pages/PipelineStatus.kt
+++ geoflow-server/src/main/kotlin/me/geoflow/web/pages/PipelineStatus.kt
@@ -1,12 +1,20 @@
 package me.geoflow.web.pages
 
+import io.ktor.application.ApplicationCall
+import kotlinx.coroutines.runBlocking
 import me.geoflow.core.database.tables.PipelineRuns
 import me.geoflow.web.html.addParamsAsJsGlobalVariables
 import kotlinx.html.FlowContent
 import kotlinx.html.STYLE
+import kotlinx.html.id
+import kotlinx.html.li
 import kotlinx.html.script
+import kotlinx.html.select
+import me.geoflow.web.api.NoBody
+import me.geoflow.web.api.makeApiCall
 import me.geoflow.web.html.basicTable
 import me.geoflow.web.html.confirmModal
+import me.geoflow.web.session
 
 /**
  * Page for pipeline status operations
@@ -20,6 +28,8 @@ import me.geoflow.web.html.confirmModal
 class PipelineStatus(
     /** Code of the workflow operation to be displayed in this page. Only displays runs currently in this state */
     private val workflowCode: String,
+    /** Call context used to extract the session */
+    private val call: ApplicationCall,
 ) : BasePage() {
 
     override val styles: STYLE.() -> Unit = {}
@@ -30,7 +40,13 @@ class PipelineStatus(
             dataUrl = "pipeline-runs/$workflowCode",
             dataField = "payload",
             clickableRows = false,
-        )
+        ) {
+            li {
+                select(classes = "custom-select") {
+                    id = STATUS_SELECT
+                }
+            }
+        }
         confirmModal(
             confirmModalId = CONFIRM_PICKUP,
             confirmMessage = "Pick up this run?",
@@ -49,12 +65,22 @@ class PipelineStatus(
     }
 
     override val script: FlowContent.() -> Unit = {
+        val operationsJson = runBlocking {
+            val session = call.session
+            requireNotNull(session)
+            makeApiCall<NoBody, String>(
+                endPoint = "/api/operations",
+                apiToken = session.apiToken,
+            )
+        }
         script {
             addParamsAsJsGlobalVariables(
                 "confirmPickupId" to CONFIRM_PICKUP,
                 "tableId" to TABLE_ID,
                 "confirmForwardId" to CONFIRM_FORWARD,
-                "confirmBackId" to CONFIRM_BACK
+                "confirmBackId" to CONFIRM_BACK,
+                "operationsJson" to operationsJson,
+                "statusSelectId" to STATUS_SELECT,
             )
         }
         script {
@@ -63,12 +89,11 @@ class PipelineStatus(
     }
 
     companion object {
-
         private const val CONFIRM_PICKUP = "confirmPickup"
         private const val CONFIRM_FORWARD = "confirmForward"
         private const val CONFIRM_BACK = "confirmBack"
         private const val TABLE_ID = "runs"
-
+        private const val STATUS_SELECT = "status"
     }
 
 }
