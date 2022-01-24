3c9cec8307431c94a74d11d5b9a3673ddeaf8306
Steven <steventhomson72@yahoo.ca>
2022-01-23 20:47:46 -0500

    Created a Domain template class for domain definition in postgresql

---
+++ geoflow-core/src/main/kotlin/me/geoflow/core/database/domains/Domain.kt
@@ -0,0 +1,17 @@
+package me.geoflow.core.database.domains
+
+/**
+ * Template for defining a Postgresql Domain. Specified the name, data_type and constraints to make a CREATE statement
+ */
+open class Domain (
+    name: String,
+    dataType: String,
+    vararg constraints: Pair<String, String>,
+) {
+    /** SQL DDL for creating the specified domain */
+    val createStatement: String = """
+        CREATE DOMAIN $name AS $dataType ${constraints.joinToString(separator = " ") { (name, expression) ->
+            "CONSTRAINT $name CHECK($expression)"
+        }}
+    """.trimIndent()
+}
