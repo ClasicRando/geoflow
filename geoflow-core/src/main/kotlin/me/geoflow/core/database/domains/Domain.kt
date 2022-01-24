7958d8c74a2092b984679dc49703678669dab690
Steven <steventhomson72@yahoo.ca>
2022-01-23 21:48:41 -0500

    Updated domain to keep the name parameter

--- geoflow-core/src/main/kotlin/me/geoflow/core/database/domains/Domain.kt
+++ geoflow-core/src/main/kotlin/me/geoflow/core/database/domains/Domain.kt
@@ -4,7 +4,8 @@ package me.geoflow.core.database.domains
  * Template for defining a Postgresql Domain. Specified the name, data_type and constraints to make a CREATE statement
  */
 open class Domain (
-    name: String,
+    /** non qualified name of the domain */
+    val name: String,
     dataType: String,
     vararg constraints: Pair<String, String>,
 ) {
