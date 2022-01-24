b968eebe8c9fc3b4ebd4c6bdf3682b2e755da532
Steven <steventhomson72@yahoo.ca>
2022-01-23 21:48:19 -0500

    Updated default data files to match new schema

--- geoflow-core/src/main/resources/pipeline_tasks.sql
+++ geoflow-core/src/main/resources/pipeline_tasks.sql
@@ -2,16 +2,30 @@ DO $$DECLARE
     collectionPipeline text[] := ARRAY[
         'Build Pipeline Run',
         'Confirm Run Instance',
+        'Download Missing Files',
         'Scan Source Folder',
         'Confirm Record Date',
         'Backup Files to Zip Folder',
         'Validate Source Tables'
     ];
+    loadPipeline text[] := ARRAY[
+        'Backup Old Tables',
+        'Check If Data Is Old',
+        'Analyze Files',
+        'Check Table Stats',
+        'Load Files',
+        'Set Loading Logic'
+    ];
 	r RECORD;
 BEGIN
-    INSERT INTO pipeline_tasks(pipeline_id,task_id,parent_task,parent_task_order)
-    SELECT t1.pipeline_id, t2.task_id, 0 parent_task, i parent_task_order
+    INSERT INTO pipeline_tasks(pipeline_id,task_id,task_order)
+    SELECT t1.pipeline_id, t2.task_id, i parent_task_order
     FROM   pipelines t1, tasks t2, (select name, row_number() over () i FROM unnest(collectionPipeline) as x(name)) t3
     WHERE  t1.name = 'Default Collection'
+    AND    t2.name = t3.name;
+    INSERT INTO pipeline_tasks(pipeline_id,task_id,task_order)
+    SELECT t1.pipeline_id, t2.task_id, i parent_task_order
+    FROM   pipelines t1, tasks t2, (select name, row_number() over () i FROM unnest(loadPipeline) as x(name)) t3
+    WHERE  t1.name = 'Default Load'
     AND    t2.name = t3.name;
 END$$;
