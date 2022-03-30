ea1885b75f282fd51cfc83faed177a3f9004b614
Steven <steventhomson72@yahoo.ca>
2022-03-30 19:56:39 -0400

    Added annotations to fields with different composite names

--- geoflow-core/src/main/kotlin/me/geoflow/core/loading/ColumnStats.kt
+++ geoflow-core/src/main/kotlin/me/geoflow/core/loading/ColumnStats.kt
@@ -9,8 +9,10 @@ import org.postgresql.util.PGobject
 data class ColumnStats(
     /** column name */
     val name: String,
+    @CompositeField("min_length")
     /** column min length of characters */
     val minLength: Int,
+    @CompositeField("max_length")
     /** column max length of characters */
     val maxLength: Int,
     /** column source data type */
